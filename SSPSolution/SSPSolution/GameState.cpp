7876fbd948b0d4a526c76981cfb9a2a699999e08
Pistonnoob <kimeld@hotmail.com>
2016-11-15 10:11:04 +0100

    Added GameState related base classes & filters

---
+++ SSPSolution/SSPSolution/GameState.cpp
@@ -0,0 +1,17 @@
+#include "GameState.h"
+#include "GameStateHandler.h"
+
+
+GameState::GameState()
+{
+}
+
+
+GameState::~GameState()
+{
+}
+
+int GameState::InitializeBase(GameStateHandler * gsh)
+{
+	return 0;
+}
