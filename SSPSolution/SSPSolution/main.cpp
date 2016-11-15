169e64f6e8ee9f044cc2096f3e0d9787adaa6e18
AceMice <Engborg@live.se>
2016-11-15 10:09:37 +0100

    ADD SFML to project
    This does not include the release version

--- SSPSolution/SSPSolution/main.cpp
+++ SSPSolution/SSPSolution/main.cpp
@@ -1,6 +1,21 @@
+#include <SFML\Window.hpp>
+
 
 int main()
 {
+	sf::Window window(sf::VideoMode(800, 600), "SFML works!");
+
+	while (window.isOpen())
+	{
+		sf::Event event;
+		while (window.pollEvent(event))
+		{
+			if (event.type == sf::Event::Closed)
+				window.close();
+		}
+
+		window.display();
+	}
 
 	return 0;
 }
