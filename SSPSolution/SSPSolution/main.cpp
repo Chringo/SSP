1dfabddf008769d067edd0bd340657d6cbb292a4
AceMice <Engborg@live.se>
2016-11-16 10:47:52 +0100

    ADD fullscreen option for sfml window, press F atm

--- SSPSolution/SSPSolution/main.cpp
+++ SSPSolution/SSPSolution/main.cpp
@@ -3,6 +3,7 @@
 
 int main()
 {
+	bool fullscreen = false;
 	sf::Window window(sf::VideoMode(800, 600), "SFML works!");
 
 	while (window.isOpen())
@@ -12,6 +13,23 @@ int main()
 		{
 			if (event.type == sf::Event::Closed)
 				window.close();
+
+			if (event.type == sf::Event::KeyPressed) {
+				if (event.key.code == sf::Keyboard::F) {
+					window.close();
+					if (fullscreen) {
+						window.create(sf::VideoMode(800, 600), "SFML works!");
+						fullscreen = false;
+					}
+					else {
+						window.create(sf::VideoMode(800, 600), "SFML works!", sf::Style::Fullscreen);
+						fullscreen = true;
+					}
+				}
+				else if (event.key.code == sf::Keyboard::Escape) {
+					window.close();
+				}
+			}
 		}
 
 		window.display();
