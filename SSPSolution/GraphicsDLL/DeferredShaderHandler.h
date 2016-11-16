43b090bbc0d3de4042a7cd47cfc0e88b12b3eeab
AceMice <Engborg@live.se>
2016-11-16 12:57:49 +0100

    ADD DeferredShaderHandler

---
+++ SSPSolution/GraphicsDLL/DeferredShaderHandler.h
@@ -0,0 +1,16 @@
+#ifndef GRAPHICSDLL_DEFERREDSHADERHANDLER
+#define GRAPHICSDLL_DEFERREDSHADERHANDLER
+
+#include "ShaderHandler.h"
+
+class DeferredShaderHandler :
+	public ShaderHandler
+{
+private:
+
+public:
+	DeferredShaderHandler();
+	~DeferredShaderHandler();
+};
+
+#endif
