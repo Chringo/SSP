f48766e9abf255a7856483898d52ee622705d202
AceMice <Engborg@live.se>
2016-11-16 12:59:56 +0100

    ADD LightShaderHandler

---
+++ SSPSolution/GraphicsDLL/LightShaderHandler.h
@@ -0,0 +1,16 @@
+#ifndef GRAPHICSDLL_LIGHTSHADERHANDLER
+#define GRAPHICSDLL_LIGHTSHADERHANDLER
+
+#include "ShaderHandler.h"
+
+class LightShaderHandler :
+	public ShaderHandler
+{
+private:
+
+public:
+	LightShaderHandler();
+	~LightShaderHandler();
+};
+
+#endif
