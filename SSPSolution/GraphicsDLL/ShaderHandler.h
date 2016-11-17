e922b4a98afa6dc71affb431be7aeafbfdea34e1
AceMice <Engborg@live.se>
2016-11-17 10:18:35 +0100

    ADD ShaderHandler virtual functions

--- SSPSolution/GraphicsDLL/ShaderHandler.h
+++ SSPSolution/GraphicsDLL/ShaderHandler.h
@@ -1,6 +1,10 @@
 #ifndef GRAPHICSDLL_SHADERHANDLER
 #define GRAPHICSDLL_SHADERHANDLER
 
+#include <d3d11.h>
+#include <DirectXMath.h>
+#incldue "ShaderLib.h"
+
 class ShaderHandler
 {
 private:
@@ -8,6 +12,10 @@ private:
 public:
 	 ShaderHandler();
 	 virtual ~ShaderHandler();
+
+	 virtual int Initialize(ID3D11Device* device, HWND* windowHandle, DirectX::XMFLOAT2 resolution) = 0;
+	 virtual int SetActive(ShaderLib::ShaderType shaderType) = 0;
+	 virtual void Shutdown() = 0;
 };
 
 #endif
