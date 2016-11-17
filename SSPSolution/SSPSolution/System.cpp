adb3cd35e31321ffbd22b19d9794783df798d7d4
Pistonnoob <kimeld@hotmail.com>
2016-11-17 12:38:51 +0100

    ADD System

---
+++ SSPSolution/SSPSolution/System.cpp
@@ -0,0 +1,12 @@
+#include "System.h"
+
+
+
+System::System()
+{
+}
+
+
+System::~System()
+{
+}
---
+++ SSPSolution/SSPSolution/System.h
@@ -0,0 +1,10 @@
+#ifndef SSPAPPLICATION_CORE_SYSTEM_H
+#define SSPAPPLICATION_CORE_SYSTEM_H
+class System
+{
+public:
+	System();
+	~System();
+};
+
+#endif
