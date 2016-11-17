f8f66e60cc1d91b974d459dcc941b7e5625ad745
AceMice <Engborg@live.se>
2016-11-17 12:22:05 +0100

    UPDATE ShaderHandler base Shutdown() implemented

--- SSPSolution/GraphicsDLL/ShaderHandler.cpp
+++ SSPSolution/GraphicsDLL/ShaderHandler.cpp
@@ -16,3 +16,35 @@ ShaderHandler::ShaderHandler()
 ShaderHandler::~ShaderHandler()
 {
 }
+
+void ShaderHandler::Shutdown()
+{
+	for (int i = 0; i < 4; i++)
+	{
+		if (this->m_vertexShader[i])
+		{
+			this->m_vertexShader[i]->Release();
+			this->m_vertexShader[i] = nullptr;
+		}
+	}
+	if (this->m_geoShader)
+	{
+		this->m_geoShader->Release();
+		this->m_geoShader = nullptr;
+	}
+	if (this->m_pixelShader)
+	{
+		this->m_pixelShader->Release();
+		this->m_pixelShader = nullptr;
+	}
+	if (this->m_layout)
+	{
+		this->m_layout->Release();
+		this->m_layout = nullptr;
+	}
+	if (this->m_matrixBuffer)
+	{
+		this->m_matrixBuffer->Release();
+		this->m_matrixBuffer = nullptr;
+	}
+}
