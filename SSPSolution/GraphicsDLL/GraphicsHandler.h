63294a31883aa9ec1569517bcfc54613b2f10a1b
AceMice <Engborg@live.se>
2016-11-18 12:18:52 +0100

    ADD GraphicsHandler.h base

--- SSPSolution/GraphicsDLL/GraphicsHandler.h
+++ SSPSolution/GraphicsDLL/GraphicsHandler.h
@@ -1,13 +1,26 @@
 #ifndef GRAPHICSDLL_GRAPHICSHANDLER
 #define GRAPHICSDLL_GRAPHICSHANDLER
 
+#include "Direct3DHandler.h"
+#include "DeferredShaderHandler.h"
+#include "LightShaderHandler.h"
+
 class GraphicsHandler
 {
 private:
+	Direct3DHandler* d3dHandler;
 
+	DeferredShaderHandler* deferredSH;
+	LightShaderHandler* lightSH;
+
+	HWND* windowHandle;
 public:
 	GraphicsHandler();
 	~GraphicsHandler();
+
+	int Initialize(HWND* windowHandle);
+	int Render();
+	void Shutdown();
 };
 
 #endif
