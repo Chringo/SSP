d3d1dd8822aec0f62c066ed75bb10babbe6a488e
Pistonnoob <kimeld@hotmail.com>
2016-11-18 15:59:51 +0100

    UPDATE System window dimensions

--- SSPSolution/SSPSolution/System.h
+++ SSPSolution/SSPSolution/System.h
@@ -4,8 +4,8 @@
 #include <iostream>
 #include <SDL_syswm.h>
 #include "InputHandler.h"
-const int SCREEN_WIDTH = 640;
-const int SCREEN_HEIGHT = 480;
+const int SCREEN_WIDTH = 1280;
+const int SCREEN_HEIGHT = 720;
 class System
 {
 private:
