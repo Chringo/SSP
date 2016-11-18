3b66257b62444dd2a75db5f890bab90d77046e24
Pistonnoob <kimeld@hotmail.com>
2016-11-18 13:11:15 +0100

    UPDATE Inputhandler Functions

--- SSPSolution/SSPSolution/InputHandler.h
+++ SSPSolution/SSPSolution/InputHandler.h
@@ -25,12 +25,28 @@ private:
 	int m_screenWidth;
 	int m_screenHeight;
 	unsigned int m_lastKeyPressed;
-	bool wasAnyKeyPressed;
+	bool m_wasAnyKeyPressed;
 
 public:
 	InputHandler();
 	~InputHandler();
+	void Initialize(HINSTANCE hInstance, HWND hwnd, int screenWidth, int screenHeight);
+	void Shutdown();
+	void Update();
 
+	void KeyDown(unsigned int key);
+	void KeyUp(unsigned int key);
+	bool isKeyPressed(unsigned int key);
+	bool isKeyDown(unsigned int key);
+	bool isKeyReleased(unsigned int key);
+
+	//0 = left, 1 = right, 2 = scroll click, 3 = "Down button" on mouse
+	bool isMouseKeyPressed(unsigned int key);
+	bool isMouseKeyDown(unsigned int key);
+	bool isMouseKeyReleased(unsigned int key);
+
+	DirectX::XMFLOAT2 getMousePos();
+	DirectX::XMFLOAT2 getMousePosInWindow();
 
 private:
 	void ReadKeyboard();
