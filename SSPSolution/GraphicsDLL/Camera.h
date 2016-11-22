2a7e91bc5834c36a427b1f51612a58a84d726dce
Pistonnoob <kimeld@hotmail.com>
2016-11-22 09:46:04 +0100

    UPDATE Camera.h functions and variables

--- SSPSolution/GraphicsDLL/Camera.h
+++ SSPSolution/GraphicsDLL/Camera.h
@@ -5,7 +5,8 @@
 class Camera
 {
 private:
-	DirectX::XMMATRIX view;
+	DirectX::XMMATRIX viewMatrix;
+	DirectX::XMMATRIX baseViewMatrix;
 	DirectX::XMVECTOR cameraPos;
 	DirectX::XMVECTOR lookAt;
 	DirectX::XMVECTOR cameraUp;
@@ -17,6 +18,33 @@ public:
 	Camera();
 	virtual ~Camera();
 
+	int Initialize();
+	int Update();
+#pragma region
+	void GetViewMatrix(DirectX::XMMATRIX& storeIn);
+	void GetBaseViewMatrix(DirectX::XMMATRIX& storeIn);
+	DirectX::XMFLOAT3 GetCameraPos();
+	DirectX::XMFLOAT3 GetLookAt();
+	DirectX::XMFLOAT3 GetCameraUp();
+	float GetRoll();
+	float GetPitch();
+	float GetYaw();
+#pragma endregion getters
+#pragma region
+	void SetCameraPos(DirectX::XMFLOAT3 newCamPos);
+	void SetCameraPos(DirectX::XMVECTOR newCamPos);
+	void SetLookAt(DirectX::XMFLOAT3 newLookAt);
+	void SetLookAt(DirectX::XMVECTOR newLookAt);
+	void SetCameraUp(DirectX::XMFLOAT3 newCamUp);
+	void SetCameraUp(DirectX::XMVECTOR newCamUp);
+
+	void SetRoll(float roll);
+	void ApplyRoll(float rollIncrease);
+	void SetPitch(float pitch);
+	void ApplyPitch(float pitchIncrease);
+	void SetYaw(float yaw);
+	void ApplyYaw(float yawIncrease);
+#pragma endregion setters
 private:
 };
 
