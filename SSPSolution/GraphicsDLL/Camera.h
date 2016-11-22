49944173e165cc8dd5804b9a8f2a7a0e58c13e40
Pistonnoob <kimeld@hotmail.com>
2016-11-22 09:37:43 +0100

    UPDATE Camera variables

--- SSPSolution/GraphicsDLL/Camera.h
+++ SSPSolution/GraphicsDLL/Camera.h
@@ -1,8 +1,18 @@
 #ifndef GRAPHICSDLL_CAMERA_H
 #define GRAPHICSDLL_CAMERA_H
+#include <DirectXMath.h>
+
 class Camera
 {
 private:
+	DirectX::XMMATRIX view;
+	DirectX::XMVECTOR cameraPos;
+	DirectX::XMVECTOR lookAt;
+	DirectX::XMVECTOR cameraUp;
+
+	float roll;
+	float pitch;
+	float yaw;
 public:
 	Camera();
 	virtual ~Camera();
