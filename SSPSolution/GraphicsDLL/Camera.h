bc340b373cf385bd95455c52131b2fe8e258e09a
Pistonnoob <kimeld@hotmail.com>
2016-11-24 12:36:28 +0100

    UPDATE Camera stability increase and appliers added
    Now stores the values as Float4 internally for stability. Added more
    methods of changing the values of the position, lookAt, cameraUp.

--- SSPSolution/GraphicsDLL/Camera.h
+++ SSPSolution/GraphicsDLL/Camera.h
@@ -13,16 +13,16 @@ class GRAPHICSDLL_API Camera
 {
 private:
 	//The second matrix used in deferred rendering
-	DirectX::XMMATRIX m_viewMatrix;
+	DirectX::XMFLOAT4X4 m_viewMatrix;
 	//The base view matrix were created with the following values
 	//Pos:		0, 0, -1
 	//LookAt:	0, 0, 0
 	//CamUp:	0, 1, 0
-	DirectX::XMMATRIX m_baseViewMatrix;
+	DirectX::XMFLOAT4X4 m_baseViewMatrix;
 	//The three vectors that defines the new coordinate system
-	DirectX::XMVECTOR m_cameraPos;
-	DirectX::XMVECTOR m_lookAt;
-	DirectX::XMVECTOR m_cameraUp;
+	DirectX::XMFLOAT4 m_cameraPos;
+	DirectX::XMFLOAT4 m_lookAt;
+	DirectX::XMFLOAT4 m_cameraUp;
 
 	//The three rotational values a 3D object is capable of
 	float m_roll;
@@ -34,28 +34,39 @@ public:
 
 	//Creates the base camera views
 	int Initialize();
-	//Update the camera view matrix based on the 6 comtained values available through the setters
+	//Create a new camera view matrix based on the 6 comtained values available through the setters.
+	//Also updates the cameraPos, lookAt and cameraUp values with the rotations in rool, pitch and yaw.
 	int Update();
 #pragma region
 	void GetViewMatrix(DirectX::XMMATRIX& storeIn);
 	void GetBaseViewMatrix(DirectX::XMMATRIX& storeIn);
 	void GetCameraPos(DirectX::XMVECTOR& storeIn);
+	void GetCameraPos(DirectX::XMFLOAT4& storeIn);
 	void GetCameraPos(DirectX::XMFLOAT3& storeIn);
 	void GetLookAt(DirectX::XMVECTOR& storeIn);
+	void GetLookAt(DirectX::XMFLOAT4& storeIn);
 	void GetLookAt(DirectX::XMFLOAT3& storeIn);
 	void GetCameraUp(DirectX::XMVECTOR& storeIn);
+	void GetCameraUp(DirectX::XMFLOAT4& storeIn);
 	void GetCameraUp(DirectX::XMFLOAT3& storeIn);
 	float GetRoll();
 	float GetPitch();
 	float GetYaw();
 #pragma endregion getters
 #pragma region
-	void SetCameraPos(DirectX::XMFLOAT3 newCamPos);
+	void SetCameraPos(DirectX::XMFLOAT4 newCamPos);
 	void SetCameraPos(DirectX::XMVECTOR newCamPos);
-	void SetLookAt(DirectX::XMFLOAT3 newLookAt);
+	void SetLookAt(DirectX::XMFLOAT4 newLookAt);
 	void SetLookAt(DirectX::XMVECTOR newLookAt);
-	void SetCameraUp(DirectX::XMFLOAT3 newCamUp);
+	void SetCameraUp(DirectX::XMFLOAT4 newCamUp);
 	void SetCameraUp(DirectX::XMVECTOR newCamUp);
+	//Additional value control methods
+	void AddToCameraPos(DirectX::XMFLOAT3 applyValue);
+	void AddToLookAt(DirectX::XMFLOAT3 applyValue);
+	void AddToCameraUp(DirectX::XMFLOAT3 applyValue);
+	void MultiplyCameraPos(DirectX::XMFLOAT3 multiplyValue);
+	void MultiplyLookAt(DirectX::XMFLOAT3 multiplyValue);
+	void MultiplyCameraUp(DirectX::XMFLOAT3 multiplyValue);
 
 	void SetRoll(float roll);
 	void ApplyRoll(float rollIncrease);
