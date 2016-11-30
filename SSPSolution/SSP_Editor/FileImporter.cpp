b1dca6d86956e78fd0a8fafddcae26327c3c0a08
David Wigelius <david.wigelius@gmail.com>
2016-11-30 09:36:09 +0100

    UPDATE: made importer class

---
+++ SSPSolution/SSP_Editor/FileImporter.cpp
@@ -0,0 +1,12 @@
+#include "FileImporter.h"
+
+
+
+FileImporter::FileImporter()
+{
+}
+
+
+FileImporter::~FileImporter()
+{
+}
---
+++ SSPSolution/SSP_Editor/FileImporter.h
@@ -0,0 +1,10 @@
+#ifndef SSPEDITOR_FILEIMPORTER_FILEIMPORTER_H
+#define SSPEDITOR_FILEIMPORTER_FILEIMPORTER_H
+
+class FileImporter
+{
+public:
+	FileImporter();
+	~FileImporter();
+};
+#endif
