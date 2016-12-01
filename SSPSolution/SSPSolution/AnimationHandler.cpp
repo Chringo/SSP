a05f9bdd6793abe01bce2530e3368c3a3302cd1f
pixet93 <erik_pettersson@outlook.com>
2016-12-01 17:43:52 +0100

    ADD: Functions for the AnimationHandler.

--- SSPSolution/SSPSolution/AnimationHandler.cpp
+++ SSPSolution/SSPSolution/AnimationHandler.cpp
@@ -1,1 +1,62 @@
 #include "AnimationHandler.h"
+
+AnimationHandler::AnimationHandler()
+{
+	this->t = 0.f;
+	this->currentAnimation = -1;
+}
+
+AnimationHandler::~AnimationHandler()
+{
+}
+
+void AnimationHandler::Update(float dt)
+{
+	/*Need to check in this function what the time in the animation is.
+	If the last frame have been reached, we should check if the time is
+	at the end of the animation and if it's a looping animation.*/
+
+	/*Pseucode*/
+	//If time is at the last frame and animation is looping.
+		//Reset the time T of the animation to zero.
+
+	//If not and still playing the animation 
+		//Increment t with the time between two frames 
+
+	//If there is a new animation, and still playing 
+		//take the latest frame old animation and blend with the first frame in new.
+		//Reset time T of the previous animation to zero. 
+
+	if (!animationStack.empty())
+	{
+	}
+}
+
+void AnimationHandler::Interpolate(int currentFrame)
+{
+}
+
+void AnimationHandler::Blend(int lastFrame, int prevState, int newState)
+{
+}
+
+void AnimationHandler::Push(int currentAnimation, bool isLooping, int startFrame, int endFrame, int duration)
+{
+	StackHolder stack;
+
+	stack.isLooping = isLooping;
+	stack.startFrame = startFrame;
+	stack.endFrame = endFrame;
+	stack.duration = duration;
+
+	animationStack.push(stack);
+}
+
+void AnimationHandler::Pop()
+{
+	if (!animationStack.empty())
+	{
+		animationStack.pop();
+	}
+}
+
--- SSPSolution/SSPSolution/AnimationHandler.h
+++ SSPSolution/SSPSolution/AnimationHandler.h
@@ -1,17 +1,54 @@
 #ifndef SSPAPPLICATION_CORE_ANIMATIONHANDLER_H
 #define SSPAPPLICATION_CORE_ANIMATIONHANDLER_H
 
+#include <stack>
+
 /*This class has the responsibility to play animations in a generic way.
 By generic it is meant for any "animatible entity" to be updated by
 this class. The result of playing each frame will update the entity of 
-the skeleton and it's hiearchy of joints.*/
+the skeleton and it's hierarchy of joints.*/
+
+struct StackHolder
+{
+	bool isLooping;
+	int startFrame;
+	int endFrame;
+	int duration;
+};
 
 class AnimationHandler
 {
+
 private:
+
+	std::stack<StackHolder> animationStack;
+
+	//Pointer to the class that holds the jointList and keyframe data.
+	/*pointer->func/data*/
+	int currentAnimation;
+	float t;
 
 
 public: 
+	AnimationHandler();
+	~AnimationHandler();
+
+	/*Update each frame in the current animation.*/
+	void Update(float dt);
+
+	/*Adds a new animation to the stack.*/
+	void Push(int currentAnimation, bool isLooping, int startFrame, int endFrame, int duration);
+
+	/*Removes the animation that is on the top of the stack.*/
+	void Pop();
+
+	/*Don't know if this is suppose to be here? Maybe this is a function
+	we call from Update(), having another class holding interpolation func?*/
+	void Interpolate(int currentFrame);
+
+	/*Maybe this is more suitable for being in another class? I might consider
+	calling this in the Update() from another class pointer.*/
+	void Blend(int lastFrame, int prevState, int newState);
 };
 
 
