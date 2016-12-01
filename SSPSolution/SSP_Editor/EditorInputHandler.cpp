3875b88d88f832413971a2155900b393c9bcff57
Murbruksprodukt <johan.ganeteg@gmail.com>
2016-12-01 11:49:31 +0100

    ADD added support for picking and subsequent system

--- SSPSolution/SSP_Editor/EditorInputHandler.cpp
+++ SSPSolution/SSP_Editor/EditorInputHandler.cpp
@@ -1,9 +1,10 @@
 #include "EditorInputHandler.h"
 
-EditorInputHandler::EditorInputHandler(HINSTANCE handleInstance, HWND handle, Camera* camera, int w, int h)
+EditorInputHandler::EditorInputHandler(HINSTANCE handleInstance, HWND handle, Camera* camera, int w, int h, GraphicsHandler* graphicshandler)
 {
 	this->m_Width = w;
 	this->m_Height = h;
+	this->m_GraphicsHandler = graphicshandler;
 	HRESULT hr = DirectInput8Create(
 		handleInstance,
 		DIRECTINPUT_VERSION,
@@ -236,6 +237,12 @@ void EditorInputHandler::detectInput(double dT)
 		rayOrigin = XMVector3TransformCoord(LocalRayOrigin, inverseCamView);
 		rayDirection = DirectX::XMVector3TransformNormal(localRayDirection, inverseCamView);
 		rayDirection = DirectX::XMVector3Normalize(rayDirection);
+		
+		float result = Intersection(rayOrigin,rayDirection);
+		if (result != FLT_MAX)
+		{
+			this->m_Picked;
+		}
 
 		//DO PICKING TEST TO FIND MESH IN SCENE
 		//GET POINTER?
@@ -250,15 +257,11 @@ void EditorInputHandler::detectInput(double dT)
 	}
 #pragma endregion
 
-
-
-
 	if (keyBoardState[DIK_F] & 0x80)
 	{
 		this->m_Camera->Initialize();
 		this->m_Camera->Update();
 	}
-
 
 
 
@@ -277,8 +280,6 @@ void EditorInputHandler::detectInput(double dT)
 			float(translateCameraY),
 			float(translateCameraZ)
 		);
-		//this->m_Camera->AddToCameraPos(posTranslation);
-		//this->m_Camera->AddToLookAt(posTranslation);
 		this->m_Camera->Update();
 	}
 
@@ -293,7 +294,117 @@ void EditorInputHandler::detectInput(double dT)
 			);
 
 		this->m_Camera->SetRotation(newRotation);
-		//this->m_Camera->ApplyRotation(newRotation);
 		this->m_Camera->Update();
 	this->m_Camera->Update();
+}
+float EditorInputHandler::Intersection(DirectX::XMVECTOR rayOrigin, DirectX::XMVECTOR rayDirection)
+{
+	//for (int i = 0; i < MODELAMT / 3; i++)
+	//{
+	//
+	//	//Triangle's vertices, V1, V2, V3
+	//	DirectX::XMVECTOR tri1V1 = DirectX::XMVectorSet(0.0f, 0.0f, 0.0f, 0.0f);
+	//	DirectX::XMVECTOR tri1V2 = DirectX::XMVectorSet(0.0f, 0.0f, 0.0f, 0.0f);
+	//	DirectX::XMVECTOR tri1V3 = DirectX::XMVectorSet(0.0f, 0.0f, 0.0f, 0.0f);
+	//
+	//
+	//	//Temporary 3d floats for each vertex.
+	//	DirectX::XMFLOAT3 _tV1, _tV2, _tV3;
+	//
+	//	_tV1.x = obj.finalVector[i * 3 + 0].x;
+	//	_tV1.y = obj.finalVector[i * 3 + 0].y;
+	//	_tV1.z = obj.finalVector[i * 3 + 0].z;
+	//
+	//	_tV2.x = obj.finalVector[i * 3 + 1].x;
+	//	_tV2.y = obj.finalVector[i * 3 + 1].y;
+	//	_tV2.z = obj.finalVector[i * 3 + 1].z;
+	//
+	//	_tV3.x = obj.finalVector[i * 3 + 2].x;
+	//	_tV3.y = obj.finalVector[i * 3 + 2].y;
+	//	_tV3.z = obj.finalVector[i * 3 + 2].z;
+	//
+	//
+	//	tri1V1 = DirectX::XMVectorSet(_tV1.x, _tV1.y, _tV1.z, 1.0f);
+	//	tri1V2 = DirectX::XMVectorSet(_tV2.x, _tV2.y, _tV2.z, 1.0f);
+	//	tri1V3 = DirectX::XMVectorSet(_tV3.x, _tV3.y, _tV3.z, 1.0f);
+	//
+	//	tri1V1 = DirectX::XMVector3TransformCoord(tri1V1, matrices.World);
+	//	tri1V2 = DirectX::XMVector3TransformCoord(tri1V2, matrices.World);
+	//	tri1V3 = DirectX::XMVector3TransformCoord(tri1V3, matrices.World);
+	//
+	//
+	//	DirectX::XMVECTOR U = DirectX::XMVectorSet(0.0f, 0.0f, 0.0f, 0.0f);
+	//	DirectX::XMVECTOR V = DirectX::XMVectorSet(0.0f, 0.0f, 0.0f, 0.0f);
+	//
+	//	DirectX::XMVECTOR faceNormal = DirectX::XMVectorSet(0.0f, 0.0f, 0.0f, 0.0f);
+	//
+	//	U = DirectX::XMVectorSubtract(tri1V2, tri1V1);
+	//	V = DirectX::XMVectorSubtract(tri1V3, tri1V1);
+	//
+	//	faceNormal = DirectX::XMVector3Cross(U, V);
+	//	faceNormal = DirectX::XMVector3Normalize(faceNormal);
+	//
+	//	DirectX::XMVECTOR triPoint = tri1V1;
+	//
+	//	//plane equation
+	//	float tri1A = DirectX::XMVectorGetX(faceNormal);
+	//	float tri1B = DirectX::XMVectorGetY(faceNormal);
+	//	float tri1C = DirectX::XMVectorGetZ(faceNormal);
+	//	float tri1D = (-tri1A*DirectX::XMVectorGetX(triPoint) - tri1B*DirectX::XMVectorGetY(triPoint) - tri1C*DirectX::XMVectorGetZ(triPoint));
+	//
+	//	//now we find wher the ray intersects with the triangles plane,
+	//
+	//	float ep1, ep2, t = 0.0f;
+	//
+	//	float planeIntersectX, planeIntersectY, planeIntersectz = 0.0f;
+	//
+	//
+	//	DirectX::XMVECTOR pointInPlane = DirectX::XMVectorSet(0.0f, 0.0f, 0.0f, 0.0f);
+	//
+	//	ep1 = (DirectX::XMVectorGetX(rayOrigin)      *   tri1A) + (DirectX::XMVectorGetY(rayOrigin)       *   tri1B) + (DirectX::XMVectorGetZ(rayOrigin)       *   tri1C);
+	//	ep2 = (DirectX::XMVectorGetX(rayDirection)   *   tri1A) + (DirectX::XMVectorGetY(rayDirection)    *   tri1B) + (DirectX::XMVectorGetZ(rayDirection)    *   tri1C);
+	//
+	//	if (ep2 != 0.0f)
+	//	{
+	//		t = -(ep1 + tri1D) / (ep2);
+	//	}
+	//	if (t > 0.0f)
+	//	{
+	//		planeIntersectX = DirectX::XMVectorGetX(rayOrigin) + DirectX::XMVectorGetX(rayDirection) * t;
+	//		planeIntersectY = DirectX::XMVectorGetY(rayOrigin) + DirectX::XMVectorGetY(rayDirection) * t;
+	//		planeIntersectz = DirectX::XMVectorGetZ(rayOrigin) + DirectX::XMVectorGetZ(rayDirection) * t;
+	//
+	//		pointInPlane = DirectX::XMVectorSet(planeIntersectX, planeIntersectY, planeIntersectz, 0.0f);
+	//
+	//		if (checkPointInTriangle(tri1V1, tri1V2, tri1V3, pointInPlane))
+	//		{
+	//			return t / 2.0f;
+	//		}
+	//	}
+	//
+	//
+	//}
+	return FLT_MAX;
+}
+bool EditorInputHandler::checkPointInTriangle(const DirectX::XMVECTOR& point, const DirectX::XMVECTOR& triangleV1, const DirectX::XMVECTOR& triangleV2, const DirectX::XMVECTOR& triangleV3)
+{
+	//Denna funktion kolla om en point är i en triangle genom att
+	DirectX::XMVECTOR collisionPoint1 = DirectX::XMVector3Cross(DirectX::XMVectorSubtract(triangleV3, triangleV2), DirectX::XMVectorSubtract(point, triangleV2));
+
+	DirectX::XMVECTOR collisionPoint2 = DirectX::XMVector3Cross(DirectX::XMVectorSubtract(triangleV3, triangleV2), DirectX::XMVectorSubtract(triangleV1, triangleV2));
+	if (DirectX::XMVectorGetX(DirectX::XMVector3Dot(collisionPoint1, collisionPoint2)) >= 0)
+	{
+		collisionPoint1 = DirectX::XMVector3Cross(DirectX::XMVectorSubtract(triangleV3, triangleV1), DirectX::XMVectorSubtract(point, triangleV1));
+		collisionPoint2 = DirectX::XMVector3Cross(DirectX::XMVectorSubtract(triangleV3, triangleV1), DirectX::XMVectorSubtract(triangleV2, triangleV1));
+		if (DirectX::XMVectorGetX(DirectX::XMVector3Dot(collisionPoint1, collisionPoint2)) >= 0)
+		{
+			collisionPoint1 = DirectX::XMVector3Cross(DirectX::XMVectorSubtract(triangleV2, triangleV1), DirectX::XMVectorSubtract(point, triangleV1));
+			collisionPoint2 = DirectX::XMVector3Cross(DirectX::XMVectorSubtract(triangleV2, triangleV1), DirectX::XMVectorSubtract(triangleV3, triangleV1));
+			if (DirectX::XMVectorGetX(DirectX::XMVector3Dot(collisionPoint1, collisionPoint2)) >= 0)
+			{
+				return true;
+			}
+		}
+	}
+	return false;
 }
