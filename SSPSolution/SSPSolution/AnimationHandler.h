392cb3347a48dae976eb4618457bcbdfa84b297a
pixet93 <erik_pettersson@outlook.com>
2016-12-01 10:35:19 +0100

    Added this class for playing animations in a generic way.

---
+++ SSPSolution/SSPSolution/AnimationHandler.h
@@ -0,0 +1,18 @@
+#ifndef SSPAPPLICATION_CORE_ANIMATIONHANDLER_H
+#define SSPAPPLICATION_CORE_ANIMATIONHANDLER_H
+
+/*This class has the responsibility to play animations in a generic way.
+By generic it is meant for any "animatible entity" to be updated by
+this class. The result of playing each frame will update the entity of 
+the skeleton and it's hiearchy of joints.*/
+
+class AnimationHandler
+{
+private:
+
+public: 
+};
+
+
+#endif
+
