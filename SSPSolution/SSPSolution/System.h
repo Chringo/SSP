73e852c6a10a8899a8299e14cbda07a93019bdc7
Tobias Kling <tobbe.kling@hotmail.com>
2016-12-01 14:42:01 +0100

    ADDED included the NetworkDLL to System.h

--- SSPSolution/SSPSolution/System.h
+++ SSPSolution/SSPSolution/System.h
@@ -7,7 +7,9 @@
 #include "../GraphicsDLL/Camera.h"
 #include "InputHandler.h"
 #include "../physicsDLL/physicsDLL/PhysicsHandler.h"
+#include "../NetworkDLL/ClientNetwork.h"
 #pragma comment (lib, "../Debug/PhysicsDLL")
+#pragma comment (lib, "../Debug/NetworkDLL")
 
 const int SCREEN_WIDTH = 1280;
 const int SCREEN_HEIGHT = 720;
@@ -29,6 +31,8 @@ private:
 	InputHandler* m_inputHandler;
 	//this is a physicsHandler
 	PhysicsHandler m_physicsHandler;
+	ClientNetwork m_Client_Network;
+
 public:
 	System();
 	~System();
