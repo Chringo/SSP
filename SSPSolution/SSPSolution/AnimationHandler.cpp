070865a80cab42e4385d042ee93623361587142f
pixet93 <erik_pettersson@outlook.com>
2016-12-02 15:39:18 +0100

    UPDATE: Filling AnimationHandler Update() with functionality for playing animations.

--- SSPSolution/SSPSolution/AnimationHandler.cpp
+++ SSPSolution/SSPSolution/AnimationHandler.cpp
@@ -2,8 +2,11 @@
 
 AnimationHandler::AnimationHandler()
 {
-	this->t = 0.f;
-	this->currentAnimation = -1;
+	this->elapsedTime = 0.0f;
+	this->currentAnimation = IDLE_STATE;
+
+	/*Initialize the stack with a default "IDLE" animation.*/
+	Push(currentAnimation, true, 0, 0, 0);
 }
 
 AnimationHandler::~AnimationHandler()
@@ -16,23 +19,77 @@ void AnimationHandler::Update(float dt)
 	If the last frame have been reached, we should check if the time is
 	at the end of the animation and if it's a looping animation.*/
 
-	/*Pseucode*/
-	//If time is at the last frame and animation is looping.
-		//Reset the time T of the animation to zero.
-
-	//If not and still playing the animation 
-		//Increment t with the time between two frames 
-
-	//If there is a new animation, and still playing 
-		//take the latest frame old animation and blend with the first frame in new.
-		//Reset time T of the previous animation to zero. 
-
-	if (!animationStack.empty())
+	bool newAnimation = false;
+	
+	/*Check if there is a current animation in the stack.*/
+	if (!animationStack.empty() || currentAnimation == IDLE_STATE)
 	{
+		/*Increment elapsedTime with the delta-time. Consider to multiply framerate here?*/
+		elapsedTime += dt;
+
+		/*If the animation reaches the last frame and the animation is looping, reset elapsedTime to ZERO.*/
+		if (elapsedTime >= animationStack.top().endFrame && animationStack.top().isLooping == true)
+		{
+			elapsedTime = 0.0f;
+		}
+
+		/*If the animation reaches the last frame and the animation is NOT looping, */
+		else if (elapsedTime >= animationStack.top().endFrame && animationStack.top().isLooping == false)
+		{
+			/*Send the elapsed time from previous animation clip, it's index and the index to the next animation.*/
+			/*Blend between animations.*/
+			Blend(elapsedTime, currentAnimation, 0); //<---- Somehow get the new animation state as input argument here. 
+
+			elapsedTime = 0.0f;
+
+			/*Remove the previous played animation clip from stack.*/
+			Pop();
+
+			/*The animation is new and not the default state.*/
+			if (newAnimation != IDLE_STATE)
+			{
+				bool isLooping;
+				int startFrame, endFrame, duration;
+
+				GetAnimationData(currentAnimation, isLooping, startFrame, endFrame, duration);
+
+				/*Push the new animation to the stack.*/
+				Push(0, isLooping, startFrame, endFrame, duration);
+			}
+
+			/*The animation is the default state.*/
+			else
+			{
+				Push(IDLE_STATE, true, 0, 0, 0);
+			}
+		}
+
+		else if (elapsedTime > animationStack.top().startFrame || elapsedTime < animationStack.top().endFrame && newAnimation == true)
+		{
+			bool isLooping;
+			int startFrame, endFrame, duration;
+			GetAnimationData(currentAnimation, isLooping, startFrame, endFrame, duration);
+			
+			Push(0, isLooping, 0, 0, 0);
+
+			Blend(elapsedTime, currentAnimation, 0);
+
+			elapsedTime = 0.0f;
+		}
+
+		/*Call this function and check if the elapsedTime is at the start, between frames or at the end.*/
+		Interpolate(elapsedTime);
 	}
+
+	/*No current animation, the physics are currently controlling the "animatible entity".*/
+	if(currentAnimation == PHYSICS_STATE)
+	{
+		/*Have some kind of function here that pauses the playing of any animation, while physics do the job.*/
+	}
+		
 }
 
-void AnimationHandler::Interpolate(int currentFrame)
+void AnimationHandler::Interpolate(float currentTime)
 {
 }
 
@@ -42,14 +99,14 @@ void AnimationHandler::Blend(int lastFrame, int prevState, int newState)
 
 void AnimationHandler::Push(int currentAnimation, bool isLooping, int startFrame, int endFrame, int duration)
 {
-	StackHolder stack;
+	this->currentAnimation = currentAnimation;
 
-	stack.isLooping = isLooping;
-	stack.startFrame = startFrame;
-	stack.endFrame = endFrame;
-	stack.duration = duration;
+	AnimationClip clip;
+	clip.isLooping = isLooping;
+	clip.startFrame = startFrame;
+	clip.endFrame = endFrame;
 
-	animationStack.push(stack);
+	animationStack.push(clip);
 }
 
 void AnimationHandler::Pop()
@@ -58,5 +115,9 @@ void AnimationHandler::Pop()
 	{
 		animationStack.pop();
 	}
+}
+
+void AnimationHandler::GetAnimationData(int animationIndex, bool & isLooping, int & startFrame, int & endFrame, int & duration)
+{
 }
 
--- SSPSolution/SSPSolution/AnimationHandler.h
+++ SSPSolution/SSPSolution/AnimationHandler.h
@@ -8,12 +8,22 @@ By generic it is meant for any "animatible entity" to be updated by
 this class. The result of playing each frame will update the entity of 
 the skeleton and it's hierarchy of joints.*/
 
-struct StackHolder
+enum AnimationStates
+{
+	PHYSICS_STATE = -1, 
+	IDLE_STATE	 =	0,
+	WALK_STATE   =	1,
+	RUN_STATE    =	2,
+	JUMP_STATE   =	3,
+	THROW_STATE  =	4
+};
+
+
+struct AnimationClip
 {
 	bool isLooping;
 	int startFrame;
-	int endFrame;
-	int duration;
+	int endFrame; 
 };
 
 class AnimationHandler
@@ -21,12 +31,12 @@ class AnimationHandler
 
 private:
 
-	std::stack<StackHolder> animationStack;
+	std::stack<AnimationClip> animationStack;
 
 	//Pointer to the class that holds the jointList and keyframe data.
 	/*pointer->func/data*/
 	int currentAnimation;
-	float t;
+	float elapsedTime;
 
 
 public: 
@@ -36,15 +46,18 @@ public:
 	/*Update each frame in the current animation.*/
 	void Update(float dt);
 
-	/*Adds a new animation to the stack.*/
-	void Push(int currentAnimation, bool isLooping, int startFrame, int endFrame, int duration);
+	/*Adds a new animation to the stack. Input arguments should come from somewhere else containing frame data.*/
+	void Push(int newAnimation, bool isLooping, int startFrame, int endFrame, int duration);
 
 	/*Removes the animation that is on the top of the stack.*/
 	void Pop();
 
+	/*Get the animation information from the "animatible entity".*/
+	void GetAnimationData(int animationIndex, bool& isLooping, int& startFrame, int& endFrame, int& duration);
+
 	/*Don't know if this is suppose to be here? Maybe this is a function
 	we call from Update(), having another class holding interpolation func?*/
-	void Interpolate(int currentFrame);
+	void Interpolate(float currentTime);
 
 	/*Maybe this is more suitable for being in another class? I might consider
 	calling this in the Update() from another class pointer.*/
