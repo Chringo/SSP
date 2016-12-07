ebe99cdc5dcb5ac59bb5ce89f94cec53ccf888ed
David Wigelius <david.wigelius@gmail.com>
2016-12-07 14:29:43 +0100

    UPDATE: made model importer

--- SSPSolution/SSP_Editor/FileImporter.cpp
+++ SSPSolution/SSP_Editor/FileImporter.cpp
@@ -69,14 +69,14 @@ void FileImporter::LoadImportedFiles()
 
 			switch (loadedObject)
 			{
+			case Resources::ResourceType::RES_MODEL:
+				break;
 			case Resources::ResourceType::RES_MESH:
 				handleMesh(m_bbf_object); //also send integer for the index so we can add the qt
 				break;
 			case Resources::ResourceType::RES_ANIMATION:
 				break;
 			case Resources::ResourceType::RES_SKELETON:
-				break;
-			case Resources::ResourceType::RES_MODEL:
 				break;
 			case Resources::ResourceType::RES_TEXTURE:
 				break;
@@ -155,4 +155,44 @@ void FileImporter::handleMesh(char * m_bbf_object)
 
 void FileImporter::handleMat(char * m_bbf_object)
 {
+}
+
+void FileImporter::handleModel(char * m_bbf_object)
+{
+	/*
+	Resources::Status res;
+	Resources::Resource::RawResourceData *res_Data = (Resources::Resource::RawResourceData*)m_bbf_object;
+
+	MeshHeader *m_meshH = (MeshHeader*)(m_bbf_object + sizeof(MainHeader));
+
+	Resources::Mesh *newMesh = new Resources::Mesh(*res_Data);
+
+
+	unsigned int * indices;
+	if (m_meshH->skeleton)
+	{
+		Resources::Mesh::VertexAnim* vertices = (Resources::Mesh::VertexAnim*)((char*)m_meshH + sizeof(MeshHeader));
+		newMesh->SetVertices(vertices, nullptr, m_meshH->numVerts, true);
+		indices = (unsigned int*)((char*)vertices + (sizeof(Resources::Mesh::VertexAnim)* m_meshH->numVerts));
+	}
+	else
+	{
+		Resources::Mesh::Vertex* vertices = (Resources::Mesh::Vertex*)((char*)m_meshH + sizeof(MeshHeader));
+		newMesh->SetVertices(vertices, nullptr, m_meshH->numVerts, true);
+		indices = (unsigned int*)((char*)vertices + (sizeof(Resources::Mesh::Vertex)* m_meshH->numVerts));
+	}
+
+	if (!newMesh->SetIndices(indices, m_meshH->indexLength, nullptr, true))
+		res = Resources::Status::ST_BUFFER_ERROR; */
+	Resources::Status res;
+	Resources::Resource::RawResourceData *res_Data = (Resources::Resource::RawResourceData*)m_bbf_object;
+
+	Resources::Model::RawModelData *raw_model_Data = (Resources::Model::RawModelData*)(m_bbf_object + sizeof(MainHeader));
+
+	Resources::Model *newModel = new Resources::Model();
+
+	newModel->Create(res_Data, raw_model_Data, true);
+
+	m_models.push_back(newModel);
+
 }
