0f463d186a408176642ee13862bf0ea1f8645cbf
AceMice <Engborg@live.se>
2016-12-07 10:41:04 +0100

    ADD functions to start and stop timers

--- SSPSolution/SSPSolution/DebugHandler.cpp
+++ SSPSolution/SSPSolution/DebugHandler.cpp
@@ -2,9 +2,31 @@
 
 DebugHandler::DebugHandler()
 {
+	QueryPerformanceFrequency(&this->m_frequency);
+	this->m_timerToEnd = 0;
 }
 
 
 DebugHandler::~DebugHandler()
 {
+}
+
+int DebugHandler::StartTimer(std::string label)
+{
+	LARGE_INTEGER currTime;
+	int result = this->m_timers.size();
+	QueryPerformanceCounter(&currTime);
+	this->m_timers.push_back(Timer(currTime));
+	this->m_labels.push_back(label);
+
+	return result;
+}
+
+int DebugHandler::EndTimer()
+{
+	LARGE_INTEGER currTime;
+	QueryPerformanceCounter(&currTime);
+	this->m_timers.at(this->m_timers.size() - this->m_timerToEnd++).endTime = currTime;
+
+	return 0;
 }
--- SSPSolution/SSPSolution/DebugHandler.h
+++ SSPSolution/SSPSolution/DebugHandler.h
@@ -8,13 +8,31 @@
 class DebugHandler
 {
 private:
-	std::vector<LARGE_INTEGER> m_timers;
+	struct Timer {
+		LARGE_INTEGER startTime;
+		LARGE_INTEGER endTime;
+		Timer(LARGE_INTEGER start)
+		{
+			this->startTime = start;
+		}
+		unsigned int GetTimeMS(LARGE_INTEGER frequency)
+		{
+			LARGE_INTEGER elapsedTime;
+			elapsedTime.QuadPart = this->startTime.QuadPart - this->endTime.QuadPart;
+			elapsedTime.QuadPart *= 1000000;
+			return elapsedTime.QuadPart / frequency.QuadPart;
+		}
+	};
+
+	LARGE_INTEGER m_frequency;
+	std::vector<Timer> m_timers;
 	std::vector<std::string> m_labels;
 	std::vector<unsigned short int> m_timerMins;
 	std::vector<unsigned short int> m_timerMaxs;
 	std::vector<float> m_customValues;
 	unsigned short int m_frameTimes[10];
 	unsigned short int m_currFrameTimesPtr;
+	unsigned short int m_timerToEnd;
 
 public:
 	DebugHandler();
