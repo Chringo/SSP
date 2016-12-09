dfb864236ca2ad63d178c13ef3e3e710b2566e8c
Pistonnoob <kimeld@hotmail.com>
2016-12-09 08:48:51 +0100

    REMOVE ComponentStructs.h structs

--- SSPSolution/SSPSolution/ComponentStructs.h
+++ SSPSolution/SSPSolution/ComponentStructs.h
@@ -7,28 +7,28 @@ enum EVENT
 {
 	COLLIDED
 };
-
-struct GraphicsComponent 
-{
-	int active;
-	int modelID;
-	int materialID;
-	DirectX::XMMATRIX worldTransform;
-	bool animated;
-};
-
-struct PhysicsComponent 
-{
-	int active;
-	bool isDynamic;
-	int entityID;
-	DirectX::XMFLOAT3 position;
-	DirectX::XMFLOAT3 velocity;
-	DirectX::XMFLOAT3 rotation;
-	DirectX::XMFLOAT3 rotationVelocity;
-	double gravityInfluence;
-	bool collides;
-	std::vector<EVENT> eventList;
+//
+//struct GraphicsComponent 
+//{
+//	int active;
+//	int modelID;
+//	int materialID;
+//	DirectX::XMMATRIX worldTransform;
+//	bool animated;
+//};
+//
+//struct PhysicsComponent 
+//{
+//	int active;
+//	bool isDynamic;
+//	int entityID;
+//	DirectX::XMFLOAT3 position;
+//	DirectX::XMFLOAT3 velocity;
+//	DirectX::XMFLOAT3 rotation;
+//	DirectX::XMFLOAT3 rotationVelocity;
+//	double gravityInfluence;
+//	bool collides;
+//	std::vector<EVENT> eventList;
 };
 
 #endif
