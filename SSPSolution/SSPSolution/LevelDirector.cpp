6a08ef689546f180fa28595e0efbe8e9740b0253
Christoffer Banheden <christofferb93@hotmail.com>
2016-12-13 10:32:30 +0100

    ADD Additional basic functions

--- SSPSolution/SSPSolution/LevelDirector.cpp
+++ SSPSolution/SSPSolution/LevelDirector.cpp
@@ -1,12 +1,33 @@
 #include "LevelDirector.h"
+#define SUCCESS 1
+#define FAIL 0
 
-
-
-LevelDirector::LevelDirector()
+FSMLevel::LevelDirector::LevelDirector(){}
+FSMLevel::LevelDirector::~LevelDirector(){}
+int FSMLevel::LevelDirector::Shutdown()
 {
+
+	return SUCCESS;
 }
 
-
-LevelDirector::~LevelDirector()
+int FSMLevel::LevelDirector::Initialize()
 {
+
+	return SUCCESS;
+}
+int FSMLevel::LevelDirector::Update(float dt)
+{
+
+	return SUCCESS;
+}
+
+int FSMLevel::LevelDirector::React(int entityID, EVENT event)
+{
+
+	return SUCCESS;
+}
+
+void FSMLevel::LevelDirector::ChangeState(States newState)
+{
+
 }
--- SSPSolution/SSPSolution/LevelDirector.h
+++ SSPSolution/SSPSolution/LevelDirector.h
@@ -1,9 +1,31 @@
 #ifndef SSPAPPLICATION_AI_LEVELDIRECTOR_H
 #define SSPAPPLICATION_AI_LEVELDIRECTOR_H
-class LevelDirector
+#include "Observer.h"
+#include <vector>
+namespace FSMLevel
 {
-public:
-	LevelDirector();
-	~LevelDirector();
-};
+	enum States
+	{
+		
+	};
+	class LevelDirector
+	{
+	private:	// Variables
+		//std::vector<"hints">
+
+	public:
+		LevelDirector();
+		~LevelDirector();
+		int Shutdown();
+
+		int Initialize();
+		int Update(float deltaTime);
+
+		int React(int entityID, EVENT event);
+
+
+	private:	// Helper functions
+		void ChangeState(States newState);
+	};
+}
 #endif
