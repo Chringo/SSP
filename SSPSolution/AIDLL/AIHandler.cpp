49832de699238f77ca5ab97823af4b83dc5930d2
Marc Andersson <galthukan@gmail.com>
2016-12-15 17:20:26 +0100

    UPDATE the behavior of the Update function

--- SSPSolution/AIDLL/AIHandler.cpp
+++ SSPSolution/AIDLL/AIHandler.cpp
@@ -36,6 +36,22 @@ int AIHandler::Update(float dt)
 		{
 			// AIComponent logic/behavior
 			// movement of e.g. platforms
+			int currentWaypoint = this->m_AIComponents.at(i)->m_currentWaypoint;
+			int nrOfWaypoint = this->m_AIComponents.at(i)->m_nrOfWaypoint;
+			int time = this->m_AIComponents.at(i)->m_time;
+
+			if (!currentWaypoint == 0 || !currentWaypoint == nrOfWaypoint)
+			{
+				// TODO More to behavior and types of behavior
+				time--;
+			}
+			else
+			{
+				if (this->m_AIComponents.at(i)->m_direction == 0)
+					this->m_AIComponents.at(i)->m_direction = 1;
+				else
+					this->m_AIComponents.at(i)->m_direction = 0;
+			}
 		}
 	}
 
@@ -60,11 +76,11 @@ AIComponent* AIHandler::CreateAIComponent(int entityID)
 	newComponent->m_speed = 0;
 	newComponent->m_direction = 0;
 	newComponent->m_currentWaypoint = 0;
-	newComponent->m_nextWaypoint = 0;
+	newComponent->m_nrOfWaypoint = 0;
 
-	for (size_t i = 0; i < 8; i++)
-	{
+	for (int i = 0; i < 8; i++) {
 		newComponent->m_waypoints[i] = DirectX::XMVECTOR();
+		newComponent->m_nrOfWaypoint++;
 	}
 
 	return newComponent;
