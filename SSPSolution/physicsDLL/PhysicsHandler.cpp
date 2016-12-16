eb3cc177a68561670483670c0e6509a73c3f8771
Apakakan <apakaka@live.com>
2016-12-16 09:32:30 +0100

    Changed masses in chain

--- SSPSolution/physicsDLL/PhysicsHandler.cpp
+++ SSPSolution/physicsDLL/PhysicsHandler.cpp
@@ -691,7 +691,7 @@ bool PhysicsHandler::Initialize()
 
 	PhysicsComponent* ptr = nullptr;
 	ptr = this->CreatePhysicsComponent(tempPos);
-	ptr->PC_mass = 10;
+	ptr->PC_mass = 20;
 	ptr->PC_is_Static = false;
 	ptr->PC_active = 1;
 	//ptr->PC_velocity = DirectX::XMVectorSet(-0.5, 0.3, 0.0, 0);
