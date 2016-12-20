88cb98d19a61d00d2af77056594454cc982ad4ad
MartinClementson <martin.clementson@outlook.com>
2016-12-20 18:27:22 +0100

    ADD: Memory leak check for editor

--- SSPSolution/SSP_Editor/main.cpp
+++ SSPSolution/SSP_Editor/main.cpp
@@ -1,8 +1,15 @@
 #include "SSP_Editor.h"
 #include <QtWidgets\qapplication.h>
 
+#define CRTDBG_MAP_ALLOC
+#include <stdlib.h>
+#include <crtdbg.h>
+#define new new(_NORMAL_BLOCK, __FILE__, __LINE__)
+
 int main(int argc, char *argv[])
 {
+	_CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
+
     QApplication a(argc, argv);
     SSP_Editor w;
     w.show();
