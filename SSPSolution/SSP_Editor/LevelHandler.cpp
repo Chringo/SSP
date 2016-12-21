b7778f7d372bd5d765d71e73a86c4f7525322394
MartinClementson <martin.clementson@outlook.com>
2016-12-21 13:16:26 +0100

    UPDATE: writing/loading header to/from file

--- SSPSolution/SSP_Editor/LevelHandler.cpp
+++ SSPSolution/SSP_Editor/LevelHandler.cpp
@@ -27,18 +27,29 @@ LevelData::LevelStatus LevelHandler::ExportLevelFile()
 
 	std::fstream file;
 	file.open(path, std::fstream::out | std::fstream::binary);
+	LevelData::MainLevelHeader header = this->GetMainHeader();
+	char* data = (char*)&header;
 
-	char hej = 'e';
-	file.write(&hej, 1);
+	file.write(data, sizeof(LevelData::MainLevelHeader));
+
 	file.close();
+
 	return LevelData::LevelStatus::L_OK;
 }
 
 LevelData::LevelStatus LevelHandler::ImportLevelFile()
 {
 	//Let the user pick a path and file name.
-	std::string fileName = GetFilePathAndName(Operation::LOAD);
+	std::string path = GetFilePathAndName(Operation::LOAD);
 
+	std::fstream file;
+	file.open(path, std::fstream::in | std::fstream::binary);
+
+	//char* data = (char*)&this->GetMainHeader();
+	LevelData::MainLevelHeader header;
+	file.read((char*)&header, sizeof(LevelData::MainLevelHeader));
+
+	file.close();
 
 	 return LevelData::LevelStatus::L_OK;
 }
@@ -46,7 +57,7 @@ LevelData::LevelStatus LevelHandler::ImportLevelFile()
 LevelData::LevelStatus LevelHandler::NewLevel()
 {
 	
-	
+	this->m_currentLevel.Destroy();
 	return LevelData::LevelStatus::L_OK;
 }
 
@@ -66,6 +77,10 @@ std::string LevelHandler::GetFilePathAndName(Operation flag)
 		dlg.setNameFilter("Levels (*.level)");
 		if (dlg.exec())
 		{
+			QFileInfo info(dlg.selectedFiles().at(0));
+			std::string hej = info.baseName().toStdString();
+			m_currentLevel.SetName(info.baseName().toStdString()); //Set the new name to the level
+			
 			return dlg.selectedFiles().at(0).toStdString();
 		}
 		else
@@ -94,4 +109,14 @@ std::string LevelHandler::GetFilePathAndName(Operation flag)
 	}
 	
 
+}
+
+LevelData::MainLevelHeader LevelHandler::GetMainHeader()
+{
+	LevelData::MainLevelHeader header;
+	
+	header.resAmount	= m_currentLevel.GetUniqueModels()->size();
+	header.entityAmount = m_currentLevel.GetNumEntities();
+	header.lightAmount  = m_currentLevel.GetNumLights();
+	return header;
 }
--- SSPSolution/SSP_Editor/LevelHandler.h
+++ SSPSolution/SSP_Editor/LevelHandler.h
@@ -4,6 +4,7 @@
 #include "LevelHeaders.h"
 #include "Level.h"
 #include <qfiledialog.h>
+#include <DataHandler.h>
 
 class LevelHandler
 {
@@ -12,12 +13,10 @@ class LevelHandler
 		SAVE
 	};
 private:
-	
-		Level m_currentLevel;
-
-		
-		QString filePath = "//DESKTOP-BOKNO6D/server/Assets/bbf files/Levels/";
+	Level m_currentLevel;
+	QString filePath = "//DESKTOP-BOKNO6D/server/Assets/bbf files/Levels/";
 	LevelHandler();
+
 public:
 	~LevelHandler();
 	static LevelHandler* GetInstance();
@@ -25,10 +24,10 @@ public:
 	LevelData::LevelStatus ExportLevelFile();
 	LevelData::LevelStatus ImportLevelFile();
 	LevelData::LevelStatus NewLevel();
-
+	Level* GetCurrentLevel() { return &this->m_currentLevel; };
 private:
 	std::string GetFilePathAndName(Operation flag);
-
-};
+	LevelData::MainLevelHeader GetMainHeader();
+ };
 
 #endif
