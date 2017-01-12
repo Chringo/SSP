b948c885cbae82b5b998a40a35deac0db32698ae
Christoffer Banheden <christofferb93@hotmail.com>
2017-01-12 15:46:16 +0100

    ADD director to work in correct scope

--- SSPSolution/SSPSolution/LevelState.h
+++ SSPSolution/SSPSolution/LevelState.h
@@ -6,11 +6,13 @@
 #include "../ResourceLib/ResourceHandler.h"
 #include "StaticEntity.h"
 #include "DynamicEntity.h"
+#include "LevelDirector.h"
 
 class LevelState :
 	public GameState
 {
 private:
+	FSMEnvironment::LevelDirector m_director;
 	Player m_player1;
 	std::vector<DynamicEntity*> m_dynamicEntitys;
 	std::vector<StaticEntity*>	m_staticEntitys;
