5d96f53539b347a7a980601487767073810bf319
Tobias Kling <tobbe.kling@hotmail.com>
2017-01-12 12:47:59 +0100

    ADD added basic code for DynamicEntity.h/cpp as a placeholder

--- SSPSolution/SSPSolution/DynamicEntity.cpp
+++ SSPSolution/SSPSolution/DynamicEntity.cpp
@@ -1,1 +1,24 @@
 #include "DynamicEntity.h"
+
+DynamicEntity::DynamicEntity()
+{
+}
+
+DynamicEntity::~DynamicEntity()
+{
+}
+
+int DynamicEntity::Initialize()
+{
+	return 0;
+}
+
+int DynamicEntity::Update(float dT, InputHandler * inputHandler)
+{
+	return 0;
+}
+
+int DynamicEntity::React(int entityID, EVENT reactEvent)
+{
+	return 0;
+}
