c554560f06179879fcdb64bec7ae9807b18a13a9
MartinClementson <martin.clementson@outlook.com>
2017-01-16 11:36:36 +0100

    ADD: Separate spawn point containers in level class

--- SSPSolution/SSP_Editor/Level.cpp
+++ SSPSolution/SSP_Editor/Level.cpp
@@ -6,29 +6,22 @@ Level::Level()
 {
 	m_ModelMap.reserve(50);
 
-	/*Container player1;
-	Container player2;
-	std::unordered_map<unsigned int, std::vector<Container>>::iterator got;
-
-
-
-		player1.internalID = 0;
-		player1.position = { 1.0f, 0.0, 0.0f };
-		player1.rotation = { 0.0f, 0.0f, 0.0f };
-		player1.isDirty = true;
-		player1.component.worldMatrix = DirectX::XMMatrixIdentity();
-		player1.component.modelID = PLAYER1;
-		this->m_ModelMap[PLAYER1].push_back(player1);
-		this->m_uniqueModels.push_back(PLAYER1);
+		m_SpawnPoints[0].internalID = 0;
+		m_SpawnPoints[0].position   = { 1.0f, 0.0, 0.0f };
+		m_SpawnPoints[0].rotation   = { 0.0f, 0.0f, 0.0f };
+		m_SpawnPoints[0].isDirty    = true;
+		m_SpawnPoints[0].component.worldMatrix = DirectX::XMMatrixTranslationFromVector(m_SpawnPoints[0].position);
+		m_SpawnPoints[0].component.modelID  = PLAYER1;
+		m_SpawnPoints[0].component.modelPtr = DataHandler::GetInstance()->GetModel(m_SpawnPoints[0].component.modelID);
 	
-		player2.internalID = 1;
-		player2.position = { -1.0f, 0.0, 0.0f };
-		player2.rotation = { 0.0f, 0.0f, 0.0f };
-		player2.isDirty = true;
-		player2.component.worldMatrix = DirectX::XMMatrixIdentity();
-		player2.component.modelID = PLAYER2;
-		this->m_ModelMap[PLAYER2].push_back(player2);
-		this->m_uniqueModels.push_back(PLAYER2);*/
+		m_SpawnPoints[1].internalID = 1;
+		m_SpawnPoints[1].position   = { -1.0f, 0.0, 0.0f };
+		m_SpawnPoints[1].rotation   = { 0.0f, 0.0f, 0.0f };
+		m_SpawnPoints[1].isDirty    = true;
+		m_SpawnPoints[1].component.worldMatrix = DirectX::XMMatrixTranslationFromVector(m_SpawnPoints[1].position);
+		m_SpawnPoints[1].component.modelID     = PLAYER2;
+		m_SpawnPoints[1].component.modelPtr = DataHandler::GetInstance()->GetModel(m_SpawnPoints[1].component.modelID);
+		
 	
 }
 
@@ -73,36 +66,15 @@ Resources::Status Level::GetModelEntity(unsigned int modelID, unsigned int insta
 
 Resources::Status Level::AddModelEntity(unsigned int modelID, unsigned int instanceID, DirectX::XMVECTOR position, DirectX::XMVECTOR rotation) // Author : Johan Ganeteg
 {
-	//if (modelID == PLAYER1 || modelID == PLAYER2)
-	//{
-	//	switch (modelID)
-	//	{
-	//	case PLAYER1:
-	//		this->m_ModelMap[modelID].at(0).position = { 1.0f, 0.0f, 0.0f };
-	//		this->m_ModelMap[modelID].at(0).rotation = { 0.0f, 0.0f, 0.0f };
-	//		this->m_ModelMap[modelID].at(0).isDirty = true;
-	//		break;
-	//	case PLAYER2:
-	//		this->m_ModelMap[modelID].at(0).position = { -1.0f, 0.0f, 0.0f };
-	//		this->m_ModelMap[modelID].at(0).rotation = { 0.0f, 0.0f, 0.0f };
-	//		this->m_ModelMap[modelID].at(0).isDirty = true;
-	//		break;
-	//	default:
-	//		break;
-	//	}
-	//	//SelectionHandler::GetInstance()->SetSelection(false);
-	//	return Resources::Status::ST_OK;
-	//}
+
 	std::unordered_map<unsigned int, std::vector<Container>>::iterator got = m_ModelMap.find(modelID);
 	std::vector<Container>* modelPtr;
 
-
-
 	Container newComponent;
 
-	newComponent.component.modelID = modelID;
-	newComponent.position = position;
-	newComponent.rotation = rotation;
+	newComponent.component.modelID  = modelID;
+	newComponent.position		    = position;
+	newComponent.rotation		    = rotation;
 	newComponent.component.modelPtr = DataHandler::GetInstance()->GetModel(modelID);
 	DirectX::XMMATRIX containerMatrix = DirectX::XMMatrixIdentity();
 
@@ -113,9 +85,7 @@ Resources::Status Level::AddModelEntity(unsigned int modelID, unsigned int insta
 	newComponent.internalID = instanceID;
 	newComponent.isDirty = true;
 
-
-
-	if (got == m_ModelMap.end()) { // if  does not exists in memory
+	if (got == m_ModelMap.end()) { // if does not exists in memory
 		this->m_ModelMap[modelID].push_back(newComponent);
 		this->m_uniqueModels.push_back(modelID);
 		return Resources::Status::ST_OK;
@@ -167,6 +137,33 @@ Resources::Status Level::UpdateModel(unsigned int modelID, unsigned int instance
 		}
 		return Resources::Status::ST_RES_MISSING;
 	}
+}
+
+Resources::Status Level::UpdateSpawnPoint(unsigned int instanceID, DirectX::XMVECTOR position, DirectX::XMVECTOR rotation)
+{
+
+	if (instanceID != 0 && instanceID != 1)
+		return Resources::Status::ST_RES_MISSING;
+
+	m_SpawnPoints[instanceID].position = position;
+	m_SpawnPoints[instanceID].rotation = rotation;
+	DirectX::XMMATRIX containerMatrix = DirectX::XMMatrixIdentity();
+
+	DirectX::XMMATRIX rotationMatrixX = DirectX::XMMatrixRotationX(DirectX::XMConvertToRadians(rotation.m128_f32[0]));
+	DirectX::XMMATRIX rotationMatrixY = DirectX::XMMatrixRotationY(DirectX::XMConvertToRadians(rotation.m128_f32[1]));
+	DirectX::XMMATRIX rotationMatrixZ = DirectX::XMMatrixRotationZ(DirectX::XMConvertToRadians(rotation.m128_f32[2]));
+	//Create the rotation matrix
+	DirectX::XMMATRIX rotationMatrix = DirectX::XMMatrixMultiply(rotationMatrixZ, rotationMatrixX);
+	rotationMatrix = DirectX::XMMatrixMultiply(rotationMatrix, rotationMatrixY);
+
+	containerMatrix = DirectX::XMMatrixMultiply(containerMatrix, rotationMatrix);
+	containerMatrix = DirectX::XMMatrixMultiply(containerMatrix, DirectX::XMMatrixTranslationFromVector(position));
+	m_SpawnPoints[instanceID].component.worldMatrix = containerMatrix;
+	m_SpawnPoints[instanceID].isDirty = false;
+	return Resources::Status::ST_OK;
+
+
+	return Resources::Status::ST_OK;
 }
 
 Resources::Status Level::RemoveModel(unsigned int modelID, unsigned int instanceID) // Author : Johan Ganeteg
@@ -234,11 +231,37 @@ unsigned int Level::GetNumLights()
 	return 0;
 }
 
+Container * Level::GetSpawnPoint(int index)
+{
+
+	for (size_t i = 0; i < 2; i++)
+	{
+		if (m_SpawnPoints[i].component.modelPtr == nullptr) // This fixes the error that the spawnpoints did not get their right models
+			m_SpawnPoints[i].component.modelPtr = DataHandler::GetInstance()->GetModel(m_SpawnPoints[i].component.modelID);
+	}
+
+	if     (index > 1)
+		return &m_SpawnPoints[1];
+	else if( index < 0)
+		return &m_SpawnPoints[0];
+	else
+		return &m_SpawnPoints[index];
+	
+}
+
 void Level::Destroy()
 {
 	m_uniqueModels.clear();
 	m_ModelMap.clear();
 	m_LightMap.clear();
 	levelName = "untitled_level";
+
+	m_SpawnPoints[0].position = { 1.0f, 0.0, 0.0f };
+	m_SpawnPoints[0].rotation = { 0.0f, 0.0f, 0.0f };
+	m_SpawnPoints[0].isDirty = true;
+
+	m_SpawnPoints[1].position = { -1.0f, 0.0, 0.0f };
+	m_SpawnPoints[1].rotation = { 0.0f, 0.0f, 0.0f };
+	m_SpawnPoints[1].isDirty = true;
 	//Ui::UiControlHandler::GetInstance()->GetAttributesHandler()->Deselect();
 }
