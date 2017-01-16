c188875da5621d56afc720b93ba8f67d53cf048e
Marc Andersson <galthukan@gmail.com>
2017-01-16 14:23:10 +0100

    UPDATE Logic for waypoint handling

--- SSPSolution/AIDLL/AIHandler.cpp
+++ SSPSolution/AIDLL/AIHandler.cpp
@@ -57,35 +57,34 @@ int AIHandler::Update(float deltaTime)
 			}
 			else if (pattern == 3)
 			{
-				//TODO Random pattern
+				//TODO Round-trip pattern
 			}
 			else
 			{
 				//Identical to pattern 2 (Circular)
 				if (direction == 0)
 				{
-					/*const int ARR_LEN = 10;
-
-					int arr[ARR_LEN] = { 0,1,2,3,4,5,6,7,8,9 };
-
-					for (int i = 0; i < ARR_LEN * 2; ++i)
-						cout << arr[i % ARR_LEN] << " ";*/
-
-					XMVECTOR waypointTo = this->m_AIComponents.at(i)->m_waypoints[i];
-					XMVECTOR waypointFrom = this->m_AIComponents.at(i)->m_waypoints[i];
-
-					// <-- vector direction
-					XMVECTOR dirVector = waypointTo - waypointFrom;
-	
-					//Should be speed dependent
-					currentWaypoint;
-					this->pos;
+					if (WaypointApprox(i))
+					{
+						currentWaypoint = this->m_AIComponents.at(i)->m_nextWaypoint;
+						this->m_AIComponents.at(i)->m_nextWaypoint++;
+						if (this->m_AIComponents.at(i)->m_nextWaypoint >= this->m_AIComponents.at(i)->m_nrOfWaypoint)
+							this->m_AIComponents.at(i)->m_nextWaypoint = 0;
+					}
 				}
 				else
 				{
-					this->pos;
+					if (WaypointApprox(i))
+					{
+						currentWaypoint = this->m_AIComponents.at(i)->m_nextWaypoint;
+						this->m_AIComponents.at(i)->m_nextWaypoint--;
+						if (this->m_AIComponents.at(i)->m_nextWaypoint <= this->m_AIComponents.at(i)->m_nrOfWaypoint)
+							this->m_AIComponents.at(i)->m_nextWaypoint = nrOfWaypoint;
+					}
 				}
 			}
+
+			//TODO Update position
 		}
 	}
 
