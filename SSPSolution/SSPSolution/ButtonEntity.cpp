604aa78a2b282044588a53b300d3e954e067800b
Pistonnoob <kimeld@hotmail.com>
2017-01-16 16:53:40 +0100

    ADD classes

---
+++ SSPSolution/SSPSolution/ButtonEntity.cpp
@@ -0,0 +1,30 @@
+#include "ButtonEntity.h"
+
+
+
+ButtonEntity::ButtonEntity()
+{
+}
+
+
+ButtonEntity::~ButtonEntity()
+{
+}
+
+int ButtonEntity::Update(float dT, InputHandler * inputHandler)
+{
+	int result = 0;
+	return result;
+}
+
+int ButtonEntity::React(int entityID, EVENT reactEvent)
+{
+	int result = 0;
+	return result;
+}
+
+int ButtonEntity::Initialize(int entityID, PhysicsComponent * pComp, GraphicsComponent * gComp)
+{
+	int result = 0;
+	return result;
+}
---
+++ SSPSolution/SSPSolution/ButtonEntity.h
@@ -0,0 +1,23 @@
+#ifndef SSPAPPLICATION_ENTITIES_BUTTONENTITY_H
+#define SSPAPPLICATION_ENTITIES_BUTTONENTITY_H
+#include "Entity.h"
+class ButtonEntity :
+	public Entity
+{
+private:
+	//Variables
+public:
+	ButtonEntity();
+	virtual ~ButtonEntity();
+
+
+	int Update(float dT, InputHandler* inputHandler);
+	int React(int entityID, EVENT reactEvent);
+
+	int Initialize(int entityID, PhysicsComponent* pComp, GraphicsComponent* gComp);
+
+private:
+	//Functions
+};
+
+#endif
