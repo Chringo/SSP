5b44fa8864cd4b80db24e4409f3cf132ffb7a35a
AceMice <Engborg@live.se>
2017-01-17 10:53:33 +0100

    UPDATE added rotation parameters and get/set isopened

--- SSPSolution/SSPSolution/DoorEntity.cpp
+++ SSPSolution/SSPSolution/DoorEntity.cpp
@@ -9,17 +9,47 @@ DoorEntity::~DoorEntity()
 {
 }
 
-int DoorEntity::Initialize(int entityID, PhysicsComponent * pComp, GraphicsComponent * gComp)
+int DoorEntity::Initialize(int entityID, PhysicsComponent * pComp, GraphicsComponent * gComp, float minRotation, float maxRotation, float rotateTime)
 {
 	this->InitializeBase(entityID, pComp, gComp);
 
 	this->m_isOpened = false;
+	this->m_minRotation = 0.f;
+	this->m_maxRotation = 3.f;
+	this->m_rotateTime = 1.f;
+	this->m_rotatePerSec = (this->m_maxRotation - this->m_minRotation) / this->m_rotateTime;
 
 	return 0;
 }
 
 int DoorEntity::Update(float dT, InputHandler * inputHandler)
 {
+	if (this->m_isOpened)
+	{
+		if (DirectX::XMVectorGetY(this->m_pComp->PC_rotation) < this->m_maxRotation)
+		{
+			this->m_pComp->PC_rotation = DirectX::XMVectorSetY(this->m_pComp->PC_rotation, DirectX::XMVectorGetY(this->m_pComp->PC_rotation) + this->m_rotatePerSec * dT);
+			if (DirectX::XMVectorGetY(this->m_pComp->PC_rotation) > this->m_maxRotation)
+			{
+				this->m_pComp->PC_rotation = DirectX::XMVectorSetY(this->m_pComp->PC_rotation, this->m_maxRotation);
+			}
+			this->SyncComponents();
+		}
+	}
+	else
+	{
+		if (DirectX::XMVectorGetY(this->m_pComp->PC_rotation) > this->m_minRotation)
+		{
+			this->m_pComp->PC_rotation = DirectX::XMVectorSetY(this->m_pComp->PC_rotation, DirectX::XMVectorGetY(this->m_pComp->PC_rotation) - this->m_rotatePerSec * dT);
+			if (DirectX::XMVectorGetY(this->m_pComp->PC_rotation) < this->m_minRotation)
+			{
+				this->m_pComp->PC_rotation = DirectX::XMVectorSetY(this->m_pComp->PC_rotation, this->m_minRotation);
+			}
+			this->SyncComponents();
+		}
+	}
+	
+
 	return 0;
 }
 
@@ -30,15 +60,24 @@ int DoorEntity::React(int entityID, EVENT reactEvent)
 		if (!this->m_isOpened)
 		{
 			this->m_isOpened = true;
-			this->m_pComp->PC_rotation = DirectX::XMVectorSet(0.f, 3.f, 0.f, 0.f);
 		} 
 		else
 		{
 			this->m_isOpened = false;
-			this->m_pComp->PC_rotation = DirectX::XMVectorSet(0.f, 0.f, 0.f, 0.f);
 		}
-		this->SyncComponents();
 	}
 
 	return 0;
+}
+
+bool DoorEntity::SetIsOpened(bool isOpened)
+{
+	bool lastValue = this->m_isOpened;
+	this->m_isOpened = isOpened;
+	return lastValue;
+}
+
+bool DoorEntity::GetIsOpened()
+{
+	return this->m_isOpened;
 }
--- SSPSolution/SSPSolution/DoorEntity.h
+++ SSPSolution/SSPSolution/DoorEntity.h
@@ -8,15 +8,22 @@ class DoorEntity :
 {
 private:
 	bool m_isOpened;
+	float m_minRotation;
+	float m_maxRotation;
+	float m_rotateTime;
+	float m_rotatePerSec;
 
 public:
 	DoorEntity();
 	virtual ~DoorEntity();
 
-	int Initialize(int entityID, PhysicsComponent* pComp, GraphicsComponent* gComp);
+	int Initialize(int entityID, PhysicsComponent* pComp, GraphicsComponent* gComp, float minRotation, float maxRotation, float rotateTime);
 
 	int Update(float dT, InputHandler* inputHandler);
 	int React(int entityID, EVENT reactEvent);
+
+	bool SetIsOpened(bool isOpened);
+	bool GetIsOpened();
 };
 
 #endif
