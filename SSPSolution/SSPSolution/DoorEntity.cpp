5b4ec1e88e0bc8f8d712fb566ccb64a9786dfc99
AceMice <Engborg@live.se>
2017-01-17 08:32:41 +0100

    ADD function declaration

--- SSPSolution/SSPSolution/DoorEntity.cpp
+++ SSPSolution/SSPSolution/DoorEntity.cpp
@@ -8,3 +8,20 @@ DoorEntity::DoorEntity()
 DoorEntity::~DoorEntity()
 {
 }
+
+int DoorEntity::Initialize(int entityID, PhysicsComponent * pComp, GraphicsComponent * gComp)
+{
+	this->InitializeBase(entityID, pComp, gComp);
+
+	return 0;
+}
+
+int DoorEntity::Update(float dT, InputHandler * inputHandler)
+{
+	return 0;
+}
+
+int DoorEntity::React(int entityID, EVENT reactEvent)
+{
+	return 0;
+}
