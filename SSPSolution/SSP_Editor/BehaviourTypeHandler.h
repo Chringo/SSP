459a50c7f5bba76cb08612c82d8c8a453c24543a
MartinClementson <martin.clementson@outlook.com>
2017-01-18 14:12:03 +0100

    Revert "ADD: BehaviourTypeHandler now adds waypoints and removes waypoint when buttons are clicked"
    This reverts commit 6a405d1876fde616d3c5cb79282ddc657d6e48f2.

--- SSPSolution/SSP_Editor/BehaviourTypeHandler.h
+++ SSPSolution/SSP_Editor/BehaviourTypeHandler.h
@@ -2,7 +2,9 @@
 #define SSPEDITOR_UI_BEHAVIOURHANDLER_H
 #include "ui_SSP_Editor.h"
 #include <qobject.h>
+#include "AIController.h"
 #include "Header.h"
+#include "LevelHandler.h"
 namespace Ui {
 	enum BehaviourType {
 		NONE = 0,
