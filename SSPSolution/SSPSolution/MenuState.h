acd4d69243c48f9bbff625d269b337eed0c47f24
AceMice <Engborg@live.se>
2017-01-19 09:11:57 +0100

    ADD ui and text comps to menustate

--- SSPSolution/SSPSolution/MenuState.h
+++ SSPSolution/SSPSolution/MenuState.h
@@ -1,10 +1,14 @@
 #ifndef SSPAPPLICATION_GAMESTATES_MENUSTATE_H
 #define SSPAPPLICATION_GAMESTATES_MENUSTATE_H
 #include "GameState.h"
+#include "../GraphicsDLL/GraphicsComponent.h"
 class MenuState :
 	public GameState
 {
 private:
+	const static int m_NR_OF_MENU_ITEMS = 2;
+	UIComponent* m_uiComps[NR_OF_MENU_ITEMS];
+	TextComponent* m_textComps[NR_OF_MENU_ITEMS];
 public:
 	MenuState();
 	~MenuState();
