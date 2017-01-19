e3755b2311200173ee35367c8165b93acd0fd5bd
AceMice <Engborg@live.se>
2017-01-19 09:17:12 +0100

    REMOVE test components from UIHandler

--- SSPSolution/GraphicsDLL/UIHandler.cpp
+++ SSPSolution/GraphicsDLL/UIHandler.cpp
@@ -29,30 +29,6 @@ void UIHandler::Initialize(ID3D11Device* device, ID3D11DeviceContext* deviceCont
 	this->m_spriteBatch = new DirectX::SpriteBatch(deviceContext);
 	this->m_spriteFont = new DirectX::SpriteFont(device, L"consolas.spritefont");
 	DirectX::CreateWICTextureFromFile(device, L"cat.png", nullptr, &this->m_texture);
-
-	this->m_UIComponents.at(0)->position = DirectX::XMFLOAT2(10.f, 10.f);
-	this->m_UIComponents.at(0)->layerDepth = 1.f;
-	this->m_UIComponents.at(0)->active = true;
-	this->m_nrOfUIComponents++;
-
-	this->m_UIComponents.at(1)->position = DirectX::XMFLOAT2(200.f, 100.f);
-	this->m_UIComponents.at(1)->scale = 0.5f;
-	this->m_UIComponents.at(1)->rotation = 2.0f;
-	this->m_UIComponents.at(1)->layerDepth = 0.f;
-	this->m_UIComponents.at(1)->active = true;
-	this->m_nrOfUIComponents++;
-
-	this->m_textComponents.at(0)->active = true;
-	this->m_textComponents.at(0)->text = L"Hello";
-	this->m_textComponents.at(0)->layerDepth = 0.5f;
-	this->m_nrOfTextComponents++;
-	
-	this->m_textComponents.at(1)->active = true;
-	this->m_textComponents.at(1)->text = L"Darkness my old friend";
-	this->m_textComponents.at(1)->position = DirectX::XMFLOAT2(50.f, 85.f);
-	this->m_textComponents.at(1)->scale = DirectX::XMFLOAT2(0.5f, 0.5f);
-	this->m_textComponents.at(1)->layerDepth = 0.5f;
-	this->m_nrOfTextComponents++;
 }
 
 void UIHandler::DrawUI()
