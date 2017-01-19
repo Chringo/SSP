ad807e6eb4d1d291149057736939e4741617efd4
AceMice <Engborg@live.se>
2017-01-19 09:17:30 +0100

    ADD Basic menu init

--- SSPSolution/SSPSolution/MenuState.cpp
+++ SSPSolution/SSPSolution/MenuState.cpp
@@ -21,6 +21,18 @@ int MenuState::Initialize(GameStateHandler * gsh, ComponentHandler* cHandler)
 {
 	int result = 0;
 	result = GameState::InitializeBase(gsh, cHandler);
+
+	for (size_t i = 0; i < m_NR_OF_MENU_ITEMS; i++)
+	{
+		this->m_uiComps[i] = cHandler->GetUIComponent();
+		this->m_uiComps[i]->position = DirectX::XMFLOAT2(100.f, 200.f + (i * 100.f));
+		this->m_textComps[i] = cHandler->GetTextComponent();
+		this->m_textComps[i]->position = DirectX::XMFLOAT2(125.f, 220.f + (i * 100.f));
+	}
+
+	this->m_textComps[0]->text = L"Start Game";
+	this->m_textComps[1]->text = L"Quit Game";
+
 	return result;
 }
 
