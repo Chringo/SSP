48a6ccf41ae5168f4f26f99d21c3bed8a898db96
Pistonnoob <kimeld@hotmail.com>
2017-01-23 13:10:53 +0100

    ADD in DoorEntityConnectionStruct

--- SSPSolution/SSPSolution/DoorEntity.h
+++ SSPSolution/SSPSolution/DoorEntity.h
@@ -2,11 +2,20 @@
 #define SSPAPPLICATION_ENTITIES_DOORENTITY_H
 
 #include "Entity.h"
+#include <vector>
 
 class DoorEntity :
 	public Entity
 {
 private:
+	struct ElementState {
+		int entityID;
+		EVENT desiredState;
+		bool desiredStateReached;
+	};
+
+	std::vector<ElementState> m_elementStates;
+	
 	bool m_isOpened;
 	float m_minRotation;
 	float m_maxRotation;
