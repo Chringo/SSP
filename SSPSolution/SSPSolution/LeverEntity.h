56debdf29e75d1e5455dab0ed8f0aa63fb323c86
Pistonnoob <kimeld@hotmail.com>
2017-01-23 15:36:08 +0100

    ADD Moved behavior from Button to Lever

--- SSPSolution/SSPSolution/LeverEntity.h
+++ SSPSolution/SSPSolution/LeverEntity.h
@@ -4,9 +4,24 @@
 class LeverEntity :
 	public Entity
 {
+private:
+	//Variables
+	int m_isActive;
+	float m_range;
 public:
 	LeverEntity();
-	~LeverEntity();
+	virtual ~LeverEntity();
+
+	int Update(float dT, InputHandler* inputHandler);
+	int React(int entityID, EVENT reactEvent);
+
+	int Initialize(int entityID, PhysicsComponent* pComp, GraphicsComponent* gComp);
+
+	//For now we check only if the player is close enough
+	int CheckPressed(DirectX::XMFLOAT3 playerPos);
+
+private:
+	//Functions
 };
 
 #endif
