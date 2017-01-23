02a059f68e356284cc5cd4b4410fc3a054ec325b
Pistonnoob <kimeld@hotmail.com>
2017-01-23 11:51:20 +0100

    ADD LeverEntity

---
+++ SSPSolution/SSPSolution/LeverEntity.cpp
@@ -0,0 +1,12 @@
+#include "LeverEntity.h"
+
+
+
+LeverEntity::LeverEntity()
+{
+}
+
+
+LeverEntity::~LeverEntity()
+{
+}
---
+++ SSPSolution/SSPSolution/LeverEntity.h
@@ -0,0 +1,12 @@
+#ifndef SSPAPPLICATION_ENTITIES_LEVERENTITY_H
+#define SSPAPPLICATION_ENTITIES_WHEELENTITY_H
+#include "Entity.h"
+class LeverEntity :
+	public Entity
+{
+public:
+	LeverEntity();
+	~LeverEntity();
+};
+
+#endif
