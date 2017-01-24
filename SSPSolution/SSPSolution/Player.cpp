3c0bf29d4a7ab6cdd07eb343428c7b38296d5f00
Sebastian Lundgren <seb.lundgren@gmail.com>
2017-01-24 16:38:48 +0100

    REMOVED more unnessesary code

--- SSPSolution/SSPSolution/Player.cpp
+++ SSPSolution/SSPSolution/Player.cpp
@@ -109,10 +109,6 @@ int Player::Update(float dT, InputHandler* inputHandler)
 				//Add the velocity to our physicsComponent
 
 				this->m_pComp->PC_velocity = DirectX::XMVectorAdd(this->m_pComp->PC_velocity, velocity);
-				else
-				{
-					int i = 0;
-				}
 			}
 
 		//}
