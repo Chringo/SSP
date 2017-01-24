c90b97b7ed17480b7b0ea7234a1edbccf6297ef6
AceMice <Engborg@live.se>
2017-01-24 10:40:21 +0100

    ADD menu button struct

--- SSPSolution/SSPSolution/MenuState.h
+++ SSPSolution/SSPSolution/MenuState.h
@@ -7,9 +7,39 @@ class MenuState :
 	public GameState
 {
 private:
+	struct MenuButton {
+		UIComponent* m_uiComp;
+		TextComponent* m_textComp;
+
+		void SetHovered(bool hovered)
+		{
+			if (hovered)
+			{
+				m_uiComp->scale = 1.25f;
+				m_textComp->scale = DirectX::XMFLOAT2(1.25f, 1.25f);
+			}
+			else
+			{
+				m_uiComp->scale = 1.f;
+				m_textComp->scale = DirectX::XMFLOAT2(1.f, 1.f);
+			}
+		}
+		void SetActive(bool active)
+		{
+			if (active)
+			{
+				m_uiComp->active = 1;
+				m_textComp->active = 1;
+			}
+			else
+			{
+				m_uiComp->active = 0;
+				m_textComp->active = 0;
+			}
+		}
+	};
 	const static int m_NR_OF_MENU_ITEMS = 2;
-	UIComponent* m_uiComps[m_NR_OF_MENU_ITEMS];
-	TextComponent* m_textComps[m_NR_OF_MENU_ITEMS];
+	MenuButton m_menuButtons[m_NR_OF_MENU_ITEMS];
 	ComponentHandler* m_cHandlerPtr;
 	Camera* m_cameraRef;
 	DirectX::XMVECTOR m_lockTarget;
