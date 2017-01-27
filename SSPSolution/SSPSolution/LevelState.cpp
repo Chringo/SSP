0dde45aa4e91c68033196cae468e8f46084dc8ed
Tobias Kling <tobbe.kling@hotmail.com>
2017-01-27 10:04:07 +0100

    UPDATE commented out zoom in zoom out for player 2

--- SSPSolution/SSPSolution/LevelState.cpp
+++ SSPSolution/SSPSolution/LevelState.cpp
@@ -878,13 +878,13 @@ int LevelState::Update(float dt, InputHandler * inputHandler)
 		if (inputHandler->IsMouseKeyPressed(SDL_BUTTON_RIGHT) && !this->m_player2.GetIsAming())
 		{
 			this->m_player2.SetAiming(true);
-			this->m_cameraRef->SetDistance(2);
+			//this->m_cameraRef->SetDistance(2);
 		}
 
 		if (inputHandler->IsMouseKeyReleased(SDL_BUTTON_RIGHT) && this->m_player2.GetIsAming())
 		{
 			this->m_player2.SetAiming(false);
-			this->m_cameraRef->SetDistance(10);
+			//this->m_cameraRef->SetDistance(10);
 		}
 
 		if (this->m_player2.GetIsAming())
