1ef9c92d7880fddb115dd492867214b8c8b4afbe
Christoffer Banheden <christofferb93@hotmail.com>
2017-02-01 11:13:47 +0100

    REMOVE State from LevelDirector since it has its own source-file

--- SSPSolution/SSPSolution/LevelDirector.h
+++ SSPSolution/SSPSolution/LevelDirector.h
@@ -6,56 +6,6 @@
 namespace FSMEnvironment
 {
 
-#pragma region temp
-	// Different types and levels of hints per state 
-	enum Hint : unsigned int
-	{
-		NONE = 0,
-		ONE,
-		TWO,
-		THREE,
-		EXAMPLE,
-		TUTORIAL
-	};
-
-	// Handling the EVENT from LevelDirector::React
-	/* This is to know which entities have been discovered/used in the level so we can trigger 
-		the next state if all these entities are part of the conditions */
-	struct Condition
-	{
-		int entityID = -1;	// Which subject
-		bool used = true;	// Have the players used the entity
-	};
-
-	class State
-	{
-	public:
-		int stateID = -1;
-		int goalStateID = 0; // all states go back to idle before entering other states
-		// TODO: Data that allows the functions to be generic
-
-		// State - Data
-		/* both players outside the "checkpoint" (bounding box) would trigger the idle state */
-		bool playerOne = false;
-		bool playerTwo = false;
-		int timeDelay = -1;
-
-		Condition subjects[5];
-
-		Hint hint1 = Hint::NONE;
-		Hint hint2 = Hint::NONE;
-		Hint hint3 = Hint::NONE;
-		
-
-		// Sub-states
-		void Initialize();	//Reset state
-		int CheckTransitions();
-		void Enter();
-		void Exit();
-		void Update(float deltaTime);
-	};
-#pragma endregion
-
 	class LevelDirector
 	{
 	private:	// Variables
@@ -72,8 +22,8 @@ namespace FSMEnvironment
 		int Initialize();		// Initiate values for new level, i.e. reset values and load new level
 		int Update(float deltaTime);	// Update LD and check state
 
-		int React(int entityID, EVENT event);
-
+		int React(int entityID, EVENT event);//TODO: Might be moved to state
+		// Add function to attach either state or LD to an entity
 
 	private:	// Helper functions
 		// TODO: 
