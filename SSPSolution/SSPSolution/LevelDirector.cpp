a7508e7844029697e08f29ad070d38984ae16c50
Marc Andersson <galthukan@gmail.com>
2017-02-03 15:01:47 +0100

    UPDATE New LD React

--- SSPSolution/SSPSolution/LevelDirector.cpp
+++ SSPSolution/SSPSolution/LevelDirector.cpp
@@ -80,16 +80,15 @@ int FSMEnvironment::LevelDirector::Update(float dt)
 int FSMEnvironment::LevelDirector::React(int entityID, EVENT event)
 {
 	// TODO: Proper reaction in director!
-
-	if (entityID == 1 && event == FIELD_CONTAINS)
-		this->m_currentState->playerOne = true;
-	else
-		this->m_currentState->playerOne = false;
-
-	if (entityID == 3 && event == FIELD_CONTAINS)
-		this->m_currentState->playerTwo = true;
-	else
-		this->m_currentState->playerTwo = false;
+	
+	for (size_t i = 0; i < 3; i++)
+	{
+		if (entityID == *this->m_currentState->fieldMap[i].FD_entityID && event == FIELD_CONTAINS)
+		{
+			*this->m_currentState->fieldMap[i].FD_first_inside = true;
+			*this->m_currentState->fieldMap[i].FD_second_inside = true;
+		}
+	}
 
 	return SUCCESS;
 }
