3b4b96dd634f7cb6e2d91b18f77cfe9c8409466b
Tobias Kling <tobbe.kling@hotmail.com>
2017-02-06 10:57:42 +0100

    UPDATE no real changes

--- SSPSolution/NetworkDLL/NetworkModule.cpp
+++ SSPSolution/NetworkDLL/NetworkModule.cpp
@@ -624,7 +624,7 @@ void NetworkModule::ReadMessagesFromClients()
 
 			case SYNC_PHYSICS:
 
-				//sPP.deserialize(&network_data[data_read]);	// Read the binary data into the object
+				sPP.deserialize(&network_data[data_read]);	// Read the binary data into the object
 
 				//this->packet_Buffer_Physic.push_back(sPP);	// Push the packet to the correct buffer
 				data_read += sizeof(SyncPhysicPacket);
