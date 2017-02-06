fbedebd4fc3a9c1bf4d39911d08a44d931dde182
MartinClementson <martin.clementson@outlook.com>
2017-02-06 14:55:47 +0100

    UPDATE:  implemented the shader code

--- SSPSolution/GraphicsDLL/Shaders/GBuffer/GBufferVS_Instanced.hlsl
+++ SSPSolution/GraphicsDLL/Shaders/GBuffer/GBufferVS_Instanced.hlsl
@@ -1,4 +1,53 @@
-float4 VS_main( float4 pos : POSITION ) : SV_POSITION
+
+cbuffer camera : register(b1)
 {
-	return pos;
+    float4x4 viewMatrix;
+    float4x4 projectionMatrix;
+
+    float4 camPos;
+    float timer,
+    padding1, padding2, padding3;
+
 }
+
+
+struct VS_IN
+    {
+    float3 Pos           : POSITION;
+    float3 Normal        : NORMAL;
+    float3 Tangent       : TANGENT0;
+    float2 UV            : TEXCOORD0;
+
+    float4x4 worldMatrix : WORLD;
+};
+
+struct VS_OUT
+{
+    float4 Pos : SV_POSITION;
+    float3 Normal : NORMAL;
+    float3 Tangent : TANGENT0;
+    float2 UV : TEXCOORD0;
+
+    float4 wPos : WORLDPOSITION;
+};
+
+
+
+
+VS_OUT VS_main(VS_IN input)
+{
+    VS_OUT output;
+
+    output.Pos   = mul(float4(input.Pos, 1.0f), input.worldMatrix);
+    output.wPos  = output.Pos;
+    output.Pos   = mul(output.Pos, viewMatrix);
+    output.Pos   = mul(output.Pos, projectionMatrix);
+
+    output.Tangent = mul(float4(input.Tangent.x, input.Tangent.y, -input.Tangent.z, 0.0f), input.worldMatrix).rgb;
+    output.Normal  = mul(float4(input.Normal, 0.0f), input.worldMatrix).rgb;
+  
+    output.UV = input.UV;
+
+    return output;
+}
+
