d1f6aa95faf95e8ccac00103c624cfa83ca6398b
Marc Andersson <galthukan@gmail.com>
2017-02-17 14:28:19 +0100

    UPDATE Pull request correction

--- SSPSolution/SSPSolution/PlatformEntity.cpp
+++ SSPSolution/SSPSolution/PlatformEntity.cpp
@@ -88,17 +88,17 @@ int PlatformEntity::React(int entityID, EVENT reactEvent)
 		this->GetAIComponent()->AC_triggered = true;
 		break;
 	case WHEEL_INCREASING:
-		printf("INCREASING\n");
+		//printf("INCREASING\n");
 		this->GetAIComponent()->AC_triggered = true;
-		this->GetAIComponent()->AC_reset = false;
+		//this->GetAIComponent()->AC_reset = false;
 		break;
 	case WHEEL_DECREASING:
-		printf("INCREASING\n");
+		//printf("INCREASING\n");
 		this->GetAIComponent()->AC_triggered = true;
 		//this->GetAIComponent()->AC_reset = false;
 		break;
 	case WHEEL_RESET:
-		printf("RESET\n");
+		//printf("RESET\n");
 		this->GetAIComponent()->AC_triggered = true;
 		//this->GetAIComponent()->AC_reset = true;
 		break;
