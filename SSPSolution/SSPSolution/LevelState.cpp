e3b5c264197d6b676ba6606b46f2cccb25e77c1e
relinkz <seb.lundgren@gmail.com>
2017-02-22 09:59:33 +0100

    test

--- SSPSolution/SSPSolution/LevelState.cpp
+++ SSPSolution/SSPSolution/LevelState.cpp
@@ -1017,21 +1017,26 @@ int LevelState::Update(float dt, InputHandler * inputHandler)
 	if (this->m_player1.isAnimationChanged())
 	{
 		AnimationComponent* ap = this->m_player1.GetAnimationComponent();
-		
+		this->m_networkModule->SendAnimationPacket(this->m_player1.GetEntityID(), ap->previousState, ap->transitionDuration, ap->blendFlag, ap->source_State->isLooping, ap->lockAnimation, ap->playingSpeed, ap->velocity);
+
 		if (ap->previousState != RAGDOLL_STATE)
 		{
-			if (this->m_player1.GetRagdoll()->state == ANIMATED_TRANSITION)
-			{
-				this->m_networkModule->SendAnimationPacket(this->m_player1.GetEntityID(), ap->previousState, ap->transitionDuration, ap->blendFlag, ap->source_State->isLooping, ap->lockAnimation, ap->playingSpeed, ap->velocity);
-				//this->m_player1.GetRagdoll()->state = ANIMATED;
-			}
-			else
-				this->m_networkModule->SendAnimationPacket(this->m_player1.GetEntityID(), ap->previousState, ap->transitionDuration, ap->blendFlag, ap->target_State->isLooping, ap->lockAnimation, ap->playingSpeed, ap->velocity);
+			//this->m_networkModule->SendAnimationPacket(this->m_player1.GetEntityID(), ap->previousState, ap->transitionDuration, ap->blendFlag, ap->source_State->isLooping, ap->lockAnimation, ap->playingSpeed, ap->velocity);
+
+			//if (this->m_player1.GetRagdoll()->state == ANIMATED_TRANSITION)
+			//{
+			//	this->m_networkModule->SendAnimationPacket(this->m_player1.GetEntityID(), ap->previousState, ap->transitionDuration, ap->blendFlag, ap->source_State->isLooping, ap->lockAnimation, ap->playingSpeed, ap->velocity);
+			//	//this->m_player1.GetRagdoll()->state = ANIMATED;
+			//}
+			//else
+			//{
+			//	this->m_networkModule->SendAnimationPacket(this->m_player1.GetEntityID(), ap->previousState, ap->transitionDuration, ap->blendFlag, ap->target_State->isLooping, ap->lockAnimation, ap->playingSpeed, ap->velocity);
+			//}
 		}
 		else
 		{
 			//this->m_networkModule->SendAnimationPacket(this->m_player1.GetEntityID(), PLAYER_IDLE, 0, Blending::NO_TRANSITION, true, false, 0.8f, 1.0f);
-			this->m_networkModule->SendAnimationPacket(this->m_player1.GetEntityID(), ap->previousState, ap->transitionDuration, ap->blendFlag, ap->source_State->isLooping, ap->lockAnimation, ap->playingSpeed, ap->velocity);
+			//this->m_networkModule->SendAnimationPacket(this->m_player1.GetEntityID(), ap->previousState, ap->transitionDuration, ap->blendFlag, ap->source_State->isLooping, ap->lockAnimation, ap->playingSpeed, ap->velocity);
 		}
 	}
 
