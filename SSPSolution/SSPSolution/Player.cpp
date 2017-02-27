413a798fc1bffc702549f9d9eb139aa0f8754ed4
relinkz <seb.lundgren@gmail.com>
2017-02-27 07:41:06 +0100

    Fix used previous code, I did wrong in the testing

--- SSPSolution/SSPSolution/Player.cpp
+++ SSPSolution/SSPSolution/Player.cpp
@@ -316,8 +316,7 @@ int Player::Update(float dT, InputHandler* inputHandler)
 			{
 				/*Set the component to play the "release ball" animation for player IDLE.*/
 				this->m_oldAnimState = this->m_aComp->previousState;
-				//SetAnimationComponent(PLAYER_IDLE, 0.50f, Blending::SMOOTH_TRANSITION, true, false, 0.8f, this->m_aComp->velocity);
-				SetAnimationComponent(PLAYER_IDLE, 0.50f, Blending::FROZEN_TRANSITION, true, false, 0.8f, this->m_aComp->velocity);
+				SetAnimationComponent(PLAYER_IDLE, 0.50f, Blending::SMOOTH_TRANSITION, true, false, 0.8f, this->m_aComp->velocity);
 
 				this->m_aComp->previousState = PLAYER_IDLE;
 
