8cf97733c8354347c8f9cf525ea25ad6446ee725
Christoffer Banheden <christofferb93@hotmail.com>
2017-03-03 14:17:39 +0100

    FIX incorrect path name for tutorial level

--- SSPSolution/SSPSolution/MenuState.cpp
+++ SSPSolution/SSPSolution/MenuState.cpp
@@ -726,7 +726,7 @@ int MenuState::Update(float dt, InputHandler * inputHandler)
 				//Push it to the gamestate stack/vector
 				//this->PushStateToStack(levelSelect);
 
-				//levelSelect->LoadLevel(std::string("../ResourceLib/AssetFiles/TutorialLevel.level"));
+				//levelSelect->LoadLevel(std::string("../ResourceLib/AssetFiles/L0P1.level"));
 
 #pragma region
 				switch (this->m_levelToHost)
@@ -753,7 +753,7 @@ int MenuState::Update(float dt, InputHandler * inputHandler)
 
 				default:
 					printf("LOAD DEFUALT\n");
-					levelSelect->LoadLevel(std::string("../ResourceLib/AssetFiles/TutorialLevel.level"), this->m_levelToHost);
+					levelSelect->LoadLevel(std::string("../ResourceLib/AssetFiles/L0P1.level"), this->m_levelToHost);
 					break;
 
 				}
@@ -911,7 +911,7 @@ void MenuState::Hosting(float dt, InputHandler* inputHandler)
 				{
 				case 0:
 					printf("LOAD LEVEL TUT\n");
-					levelSelect->LoadLevel(std::string("../ResourceLib/AssetFiles/TutorialLevel.level"));
+					levelSelect->LoadLevel(std::string("../ResourceLib/AssetFiles/L0P1.level"));
 					break;
 
 				case 1:
@@ -931,7 +931,7 @@ void MenuState::Hosting(float dt, InputHandler* inputHandler)
 
 				default:
 					printf("LOAD DEFUALT\n");
-					levelSelect->LoadLevel(std::string("../ResourceLib/AssetFiles/TutorialLevel.level"));
+					levelSelect->LoadLevel(std::string("../ResourceLib/AssetFiles/L0P1.level"));
 					break;
 
 				}
@@ -986,7 +986,7 @@ void MenuState::Joining(InputHandler* inputHandler)
 			{
 			case 0:
 				printf("LOAD LEVEL 0\n");
-				levelSelect->LoadLevel(std::string("../ResourceLib/AssetFiles/TutorialLevel.level"), 0);
+				levelSelect->LoadLevel(std::string("../ResourceLib/AssetFiles/L0P1.level"), 0);
 				break;
 
 			case 1:
