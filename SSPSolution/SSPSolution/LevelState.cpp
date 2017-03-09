fc309473d30c0edba29c73c9099c2bd69c6167f4
relinkz <seb.lundgren@gmail.com>
2017-03-09 14:02:33 +0100

    REMOVE offset on spawn

--- SSPSolution/SSPSolution/LevelState.cpp
+++ SSPSolution/SSPSolution/LevelState.cpp
@@ -1482,7 +1482,6 @@ int LevelState::CreateLevel(LevelData::Level * data)
 			DirectX::XMVectorAdd(this->m_player1.GetPhysicsComponent()->PC_pos, DirectX::XMVectorSet(10, 0, 0, 0)),
 			this->m_player1.GetPhysicsComponent(),
 			this->m_player1.GetBall()->GetPhysicsComponent());
-
 		this->m_player2.GetPhysicsComponent()->PC_pos = this->m_player1_Spawn;
 	}
 #pragma endregion Network
